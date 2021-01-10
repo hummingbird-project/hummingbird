902daeb4dbd17d307e774c2994b58c05c6a00bb3
Adam Fowler <adamfowler71@gmail.com>
2021-01-10 15:21:45 +0000

    Add query test, improve others

--- Tests/HummingBirdTests/ApplicationTests.swift
+++ Tests/HummingBirdTests/ApplicationTests.swift
@@ -15,80 +15,102 @@ final class ApplicationTests: XCTestCase {
         XCTAssertEqual(Environment["TEST_ENV"], nil)
     }
 
-    func testGetRoute() {
+    func createApp() -> Application {
         let app = Application()
         app.router.get("/hello") { request -> EventLoopFuture<ByteBuffer> in
-            let buffer = request.allocator.buffer(string: "Hello")
+            let buffer = request.allocator.buffer(string: "GET: Hello")
             return request.eventLoop.makeSucceededFuture(buffer)
         }
+        app.router.post("/hello") { request -> EventLoopFuture<ByteBuffer> in
+            let buffer = request.allocator.buffer(string: "POST: Hello")
+            return request.eventLoop.makeSucceededFuture(buffer)
+        }
+        app.router.get("/query") { request -> EventLoopFuture<ByteBuffer> in
+            let buffer = request.allocator.buffer(string: request.uri.queryParameters["test"].map { String($0) } ?? "")
+            return request.eventLoop.makeSucceededFuture(buffer)
+        }
+        return app
+    }
+
+    func shutdownApp(_ app: Application) {
+        app.lifecycle.shutdown()
+        app.lifecycle.wait()
+    }
+
+    func testGetRoute() {
+        let app = createApp()
+        defer { shutdownApp(app) }
         DispatchQueue.global().async {
             app.serve()
         }
 
         let client = HTTPClient(eventLoopGroupProvider: .createNew)
         defer { XCTAssertNoThrow(try client.syncShutdown()) }
-        let request = HTTPClient.Request(uri: "http://localhost:8080/hello", method: .GET, headers: [:])
+        let request = HTTPClient.Request(uri: "http://localhost:8000/hello", method: .GET, headers: [:])
         let response = client.execute(request)
             .flatMapThrowing { response in
                 guard var body = response.body else { throw ApplicationTestError.noBody }
                 let string = body.readString(length: body.readableBytes)
                 XCTAssertEqual(response.status, .ok)
-                XCTAssertEqual(string, "Hello")
+                XCTAssertEqual(string, "GET: Hello")
             }
         XCTAssertNoThrow(try response.wait())
-
-        app.lifecycle.shutdown()
-        app.lifecycle.wait()
     }
 
     func testPostRoute() {
-        let app = Application()
-        app.router.post("/hello") { request -> EventLoopFuture<ByteBuffer> in
-            let buffer = request.allocator.buffer(string: "Hello")
-            return request.eventLoop.makeSucceededFuture(buffer)
-        }
+        let app = createApp()
+        defer { shutdownApp(app) }
         DispatchQueue.global().async {
             app.serve()
         }
 
         let client = HTTPClient(eventLoopGroupProvider: .createNew)
         defer { XCTAssertNoThrow(try client.syncShutdown()) }
-        let request = HTTPClient.Request(uri: "http://localhost:8080/hello", method: .POST, headers: [:])
+        let request = HTTPClient.Request(uri: "http://localhost:8000/hello", method: .POST, headers: [:])
         let response = client.execute(request)
             .flatMapThrowing { response in
                 guard var body = response.body else { throw ApplicationTestError.noBody }
                 let string = body.readString(length: body.readableBytes)
                 XCTAssertEqual(response.status, .ok)
-                XCTAssertEqual(string, "Hello")
+                XCTAssertEqual(string, "POST: Hello")
             }
         XCTAssertNoThrow(try response.wait())
-
-        app.lifecycle.shutdown()
-        app.lifecycle.wait()
-
     }
 
-    func testWrongMethodRoute() {
-        let app = Application()
-        app.router.post("/hello") { request -> EventLoopFuture<ByteBuffer> in
-            let buffer = request.allocator.buffer(string: "Hello")
-            return request.eventLoop.makeSucceededFuture(buffer)
-        }
+    func testQueryRoute() {
+        let app = createApp()
+        defer { shutdownApp(app) }
         DispatchQueue.global().async {
             app.serve()
         }
 
         let client = HTTPClient(eventLoopGroupProvider: .createNew)
         defer { XCTAssertNoThrow(try client.syncShutdown()) }
-        let request = HTTPClient.Request(uri: "http://localhost:8080/hello", method: .GET, headers: [:])
+        let request = HTTPClient.Request(uri: "http://localhost:8000/query?test=test%20data", method: .GET, headers: [:])
         let response = client.execute(request)
             .flatMapThrowing { response in
-                XCTAssertEqual(response.status, .internalServerError)
+                guard var body = response.body else { throw ApplicationTestError.noBody }
+                let string = body.readString(length: body.readableBytes)
+                XCTAssertEqual(response.status, .ok)
+                XCTAssertEqual(string, "test%20data")
             }
         XCTAssertNoThrow(try response.wait())
+    }
 
-        app.lifecycle.shutdown()
-        app.lifecycle.wait()
+    func testWrongMethodRoute() {
+        let app = createApp()
+        defer { shutdownApp(app) }
+        DispatchQueue.global().async {
+            app.serve()
+        }
 
+        let client = HTTPClient(eventLoopGroupProvider: .createNew)
+        defer { XCTAssertNoThrow(try client.syncShutdown()) }
+        let request = HTTPClient.Request(uri: "http://localhost:8000/hello2", method: .GET, headers: [:])
+        let response = client.execute(request)
+            .flatMapThrowing { response in
+                XCTAssertEqual(response.status, .notFound)
+            }
+        XCTAssertNoThrow(try response.wait())
     }
 }
