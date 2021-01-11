d732493829171074498398281334e68816e4d34a
Adam Fowler <adamfowler71@gmail.com>
2021-01-11 15:06:19 +0000

    Add swift-nio-extras

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .executable(name: "test-framework", targets: ["test-framework"]),
         .library(name: "HummingBird", targets: ["HummingBird"]),
         .library(name: "HBFileMiddleware", targets: ["HBFileMiddleware"]),
-        .library(name: "HBHTTPClient", targets: ["HBHTTPClient"]),
         .library(name: "HBJSON", targets: ["HBJSON"]),
         .library(name: "HBURLEncodedForm", targets: ["HBURLEncodedForm"]),
         .library(name: "HBXML", targets: ["HBXML"]),
@@ -19,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.16.1"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.8.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.6"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
         .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.11.1"),
@@ -39,17 +38,12 @@ let package = Package(
             .product(name: "LifecycleNIOCompat", package: "swift-service-lifecycle"),
             .product(name: "Logging", package: "swift-log"),
             .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOExtras", package: "swift-nio-extras"),
             .product(name: "NIOHTTP1", package: "swift-nio"),
         ]),
         .target(name: "HBFileMiddleware", dependencies: [
             .byName(name: "HummingBird"),
             .product(name: "NIO", package: "swift-nio"),
-        ]),
-        .target(name: "HBHTTPClient", dependencies: [
-            .byName(name: "HummingBird"),
-            .product(name: "NIO", package: "swift-nio"),
-            .product(name: "NIOHTTP1", package: "swift-nio"),
-            .product(name: "NIOSSL", package: "swift-nio-ssl"),
         ]),
         .target(name: "HBJSON", dependencies: [
             .byName(name: "HummingBird"),
@@ -65,7 +59,9 @@ let package = Package(
             .product(name: "XMLCoder", package: "XMLCoder")
         ]),
         // test targets
-        .testTarget(name: "HummingBirdTests", dependencies: ["HummingBird", "HBHTTPClient", .product(name: "AsyncHTTPClient", package: "async-http-client")]),
-        .testTarget(name: "HBHTTPClientTests", dependencies: ["HBHTTPClient"]),
+        .testTarget(name: "HummingBirdTests", dependencies: [
+                        "HummingBird",
+                        .product(name: "AsyncHTTPClient", package: "async-http-client")
+        ]),
     ]
 )
