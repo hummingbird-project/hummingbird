614ff90cd32e4014c4a7b44c3e0a1a76085458d4
Adam Fowler <adamfowler71@gmail.com>
2021-01-18 16:53:29 +0000

    Remove XML and test framework

--- Package.swift
+++ Package.swift
@@ -7,12 +7,10 @@ let package = Package(
     name: "hummingbird",
     platforms: [.macOS(.v10_15)],
     products: [
-        .executable(name: "test-framework", targets: ["test-framework"]),
         .library(name: "HummingBird", targets: ["HummingBird"]),
         .library(name: "HummingBirdFiles", targets: ["HummingBirdFiles"]),
         .library(name: "HummingBirdJSON", targets: ["HummingBirdJSON"]),
         .library(name: "HummingBirdTLS", targets: ["HummingBirdTLS"]),
-        .library(name: "HummingBirdXML", targets: ["HummingBirdXML"]),
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
@@ -22,16 +20,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.4.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.6"),
-        .package(url: "https://github.com/adam-fowler/xml-coding.git", from: "0.4.0"),
     ],
     targets: [
-        .target(name: "test-framework", dependencies: [
-            .byName(name: "HummingBird"),
-            .byName(name: "HummingBirdFiles"),
-            .byName(name: "HummingBirdJSON"),
-            .byName(name: "HummingBirdTLS"),
-            .byName(name: "HummingBirdXML"),
-        ]),
         .target(name: "CURLParser", dependencies: []),
         .target(name: "HummingBird", dependencies: [
             .product(name: "Backtrace", package: "swift-backtrace"),
@@ -55,11 +45,6 @@ let package = Package(
             .byName(name: "HummingBird"),
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "NIOSSL", package: "swift-nio-ssl"),
-        ]),
-        .target(name: "HummingBirdXML", dependencies: [
-            .byName(name: "HummingBird"),
-            .product(name: "NIOFoundationCompat", package: "swift-nio"),
-            .product(name: "XMLCoding", package: "xml-coding")
         ]),
         // test targets
         .testTarget(name: "HummingBirdTests", dependencies: [
