480f1132789863e3cd0ebcef9ed509fc675a1d62
Adam Fowler <adamfowler71@gmail.com>
2021-01-20 12:55:05 +0000

    Allow response to be extended

--- Sources/Hummingbird/Response/Response.swift
+++ Sources/Hummingbird/Response/Response.swift
@@ -2,14 +2,20 @@ import NIO
 import NIOHTTP1
 
 /// HTTP Response
-public struct Response {
+public class Response {
+    /// response status
     public var status: HTTPResponseStatus
+    /// response headers
     public var headers: HTTPHeaders
+    /// response body
     public var body: ResponseBody
+    /// Response extensions
+    public var extensions: Extensions<Response>
 
     public init(status: HTTPResponseStatus, headers: HTTPHeaders, body: ResponseBody) {
         self.status = status
         self.headers = headers
         self.body = body
+        self.extensions = Extensions()
     }
 }
