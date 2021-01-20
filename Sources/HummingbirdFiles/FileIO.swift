f4dbd536f30ba927bd4a8d8819444d0033ccc52f
Adam Fowler <adamfowler71@gmail.com>
2021-01-20 12:33:05 +0000

    Separate file handling from FileMiddleware
     so it can be used separate from middleware

---
+++ Sources/HummingbirdFiles/FileIO.swift
@@ -0,0 +1,89 @@
+import Hummingbird
+import NIO
+
+public struct FileIO {
+    let fileIO: NonBlockingFileIO
+    let chunkSize: Int
+
+    public init(application: Application) {
+        self.fileIO = .init(threadPool: application.threadPool)
+        self.chunkSize = NonBlockingFileIO.defaultChunkSize
+    }
+
+    public func headFile(for request: Request, path: String) -> EventLoopFuture<Response> {
+        return fileIO.openFile(path: path, eventLoop: request.eventLoop).flatMap { handle, region in
+            request.logger.debug("[FileIO] HEAD", metadata: ["file": .string(path)])
+            let headers: HTTPHeaders = ["content-length": region.readableBytes.description]
+            let response = Response(status: .ok, headers: headers, body: .empty)
+            try? handle.close()
+            return request.eventLoop.makeSucceededFuture(response)
+        }.flatMapErrorThrowing { _ in
+            throw HTTPError(.notFound)
+        }
+    }
+
+    public func loadFile(for request: Request, path: String) -> EventLoopFuture<Response> {
+        return fileIO.openFile(path: path, eventLoop: request.eventLoop).flatMap { handle, region in
+            request.logger.debug("[FileIO] GET", metadata: ["file": .string(path)])
+            let futureResponse: EventLoopFuture<Response>
+            if region.readableBytes > self.chunkSize {
+                futureResponse = streamFile(for: request, handle: handle, region: region)
+            } else {
+                futureResponse = loadFile(for: request, handle: handle, region: region)
+            }
+            return futureResponse
+        }.flatMapErrorThrowing { _ in
+            throw HTTPError(.notFound)
+        }
+    }
+
+    public func loadFile(for request: Request, handle: NIOFileHandle, region: FileRegion) -> EventLoopFuture<Response> {
+        return self.fileIO.read(fileHandle: handle, byteCount: region.readableBytes, allocator: request.allocator, eventLoop: request.eventLoop).map { buffer in
+            return Response(status: .ok, headers: [:], body: .byteBuffer(buffer))
+        }
+        .always { _ in
+            try? handle.close()
+        }
+    }
+
+    public func streamFile(for request: Request, handle: NIOFileHandle, region: FileRegion) -> EventLoopFuture<Response> {
+        let fileStreamer = FileStreamer(
+            handle: handle,
+            fileSize: region.readableBytes,
+            fileIO: self.fileIO,
+            chunkSize: self.chunkSize,
+            allocator: request.allocator
+        )
+        let response = Response(status: .ok, headers: [:], body: .stream(fileStreamer))
+        return request.eventLoop.makeSucceededFuture(response)
+    }
+
+    // class used to stream files
+    class FileStreamer: ResponseBodyStreamer {
+        let chunkSize: Int
+        var handle: NIOFileHandle
+        var bytesLeft: Int
+        var fileIO: NonBlockingFileIO
+        var allocator: ByteBufferAllocator
+
+        init(handle: NIOFileHandle, fileSize: Int, fileIO: NonBlockingFileIO, chunkSize: Int, allocator: ByteBufferAllocator) {
+            self.handle = handle
+            self.bytesLeft = fileSize
+            self.fileIO = fileIO
+            self.chunkSize = chunkSize
+            self.allocator = allocator
+        }
+
+        func read(on eventLoop: EventLoop) -> EventLoopFuture<ResponseBody.StreamResult> {
+            let bytesToRead = min(self.chunkSize, self.bytesLeft)
+            if bytesToRead > 0 {
+                self.bytesLeft -= bytesToRead
+                return self.fileIO.read(fileHandle: self.handle, byteCount: bytesToRead, allocator: self.allocator, eventLoop: eventLoop)
+                    .map { .byteBuffer($0) }
+            } else {
+                try? self.handle.close()
+                return eventLoop.makeSucceededFuture(.end)
+            }
+        }
+    }
+}
--- Sources/HummingbirdFiles/FileMiddleware.swift
+++ Sources/HummingbirdFiles/FileMiddleware.swift
@@ -4,17 +4,15 @@ import NIO
 
 public struct FileMiddleware: Middleware {
     let rootFolder: String
-    let fileIO: NonBlockingFileIO
-    let chunkSize: Int
+    let fileIO: FileIO
 
-    public init(_ rootFolder: String = "public", app: Application) {
+    public init(_ rootFolder: String = "public", application: Application) {
         var rootFolder = rootFolder
         if rootFolder.last == "/" {
             rootFolder = String(rootFolder.dropLast())
         }
         self.rootFolder = rootFolder
-        self.fileIO = .init(threadPool: app.threadPool)
-        self.chunkSize = NonBlockingFileIO.defaultChunkSize
+        self.fileIO = .init(application: application)
     }
 
     public func apply(to request: Request, next: RequestResponder) -> EventLoopFuture<Response> {
@@ -36,80 +34,13 @@ public struct FileMiddleware: Middleware {
 
             switch request.method {
             case .GET:
-                return fileIO.openFile(path: fullPath, eventLoop: request.eventLoop).flatMap { handle, region in
-                    request.logger.debug("[FileMiddleware] GET", metadata: ["file": .string(path)])
-                    let futureResponse: EventLoopFuture<Response>
-                    if region.readableBytes > self.chunkSize {
-                        futureResponse = streamFile(for: request, handle: handle, region: region)
-                    } else {
-                        futureResponse = loadFile(for: request, handle: handle, region: region)
-                    }
-                    return futureResponse
-                }.flatMapErrorThrowing { _ in
-                    throw error
-                }
+                return fileIO.loadFile(for: request, path: fullPath)
+
             case .HEAD:
-                return fileIO.openFile(path: fullPath, eventLoop: request.eventLoop).flatMap { handle, region in
-                    request.logger.debug("[FileMiddleware] HEAD", metadata: ["file": .string(path)])
-                    let headers: HTTPHeaders = ["content-length": region.readableBytes.description]
-                    let response = Response(status: .ok, headers: headers, body: .empty)
-                    try? handle.close()
-                    return request.eventLoop.makeSucceededFuture(response)
-                }.flatMapErrorThrowing { _ in
-                    throw error
-                }
+                return fileIO.headFile(for: request, path: fullPath)
+
             default:
                 return request.eventLoop.makeFailedFuture(error)
-            }
-        }
-    }
-
-    public func loadFile(for request: Request, handle: NIOFileHandle, region: FileRegion) -> EventLoopFuture<Response> {
-        return self.fileIO.read(fileHandle: handle, byteCount: region.readableBytes, allocator: request.allocator, eventLoop: request.eventLoop).map { buffer in
-            return Response(status: .ok, headers: [:], body: .byteBuffer(buffer))
-        }
-        .always { _ in
-            try? handle.close()
-        }
-    }
-
-    public func streamFile(for request: Request, handle: NIOFileHandle, region: FileRegion) -> EventLoopFuture<Response> {
-        let fileStreamer = FileStreamer(
-            handle: handle,
-            fileSize: region.readableBytes,
-            fileIO: self.fileIO,
-            chunkSize: self.chunkSize,
-            allocator: request.allocator
-        )
-        let response = Response(status: .ok, headers: [:], body: .stream(fileStreamer))
-        return request.eventLoop.makeSucceededFuture(response)
-    }
-
-    // class used to stream files
-    class FileStreamer: ResponseBodyStreamer {
-        let chunkSize: Int
-        var handle: NIOFileHandle
-        var bytesLeft: Int
-        var fileIO: NonBlockingFileIO
-        var allocator: ByteBufferAllocator
-
-        init(handle: NIOFileHandle, fileSize: Int, fileIO: NonBlockingFileIO, chunkSize: Int, allocator: ByteBufferAllocator) {
-            self.handle = handle
-            self.bytesLeft = fileSize
-            self.fileIO = fileIO
-            self.chunkSize = chunkSize
-            self.allocator = allocator
-        }
-
-        func read(on eventLoop: EventLoop) -> EventLoopFuture<ResponseBody.StreamResult> {
-            let bytesToRead = min(self.chunkSize, self.bytesLeft)
-            if bytesToRead > 0 {
-                self.bytesLeft -= bytesToRead
-                return self.fileIO.read(fileHandle: self.handle, byteCount: bytesToRead, allocator: self.allocator, eventLoop: eventLoop)
-                    .map { .byteBuffer($0) }
-            } else {
-                try? self.handle.close()
-                return eventLoop.makeSucceededFuture(.end)
             }
         }
     }
