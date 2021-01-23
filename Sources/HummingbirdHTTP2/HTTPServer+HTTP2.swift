ace1e1e7702f7d616797919a3b988ab4e97a29a7
Adam Fowler <adamfowler71@gmail.com>
2021-01-23 16:11:49 +0000

    Split ChannelInitializers into separate file

--- Sources/HummingbirdHTTP2/HTTPServer+HTTP2.swift
+++ Sources/HummingbirdHTTP2/HTTPServer+HTTP2.swift
@@ -1,42 +1,14 @@
 import HummingbirdCore
-import HummingbirdTLS
 import NIO
 import NIOHTTP2
 import NIOSSL
 
-struct HTTP2ChannelInitializer: HBChannelInitializer {
-    init() {}
-
-    func initialize(_ server: HBHTTPServer, channel: Channel, responder: HBHTTPResponder) -> EventLoopFuture<Void> {
-        return channel.configureHTTP2Pipeline(mode: .server) { streamChannel -> EventLoopFuture<Void> in
-            return streamChannel.pipeline.addHandler(HTTP2FramePayloadToHTTP1ServerCodec()).flatMap { () -> EventLoopFuture<Void> in
-                server.addChildHandlers(channel: streamChannel, responder: responder)
-            }
-            .map { _ in }
-        }
-        .map { _ in }
-    }
-}
-
-struct HTTP2UpgradeChannelInitializer: HBChannelInitializer {
-    let http1 = HTTP1ChannelInitializer()
-    let http2 = HTTP2ChannelInitializer()
-
-    init() { }
-
-    func initialize(_ server: HBHTTPServer, channel: Channel, responder: HBHTTPResponder) -> EventLoopFuture<Void> {
-        channel.configureHTTP2SecureUpgrade(
-            h2ChannelConfigurator: { channel in
-                http2.initialize(server, channel: channel, responder: responder)
-            },
-            http1ChannelConfigurator: { channel in
-                http1.initialize(server, channel: channel, responder: responder)
-            }
-        )
-    }
-}
-
 extension HBHTTPServer {
+    /// Add HTTP2 secure upgrade handler
+    ///
+    /// HTTP2 secure upgrade requires a TLS connection so this will add a TLS handler as well, so do not call `addTLS()` as well as this.
+    ///
+    /// - Parameter tlsConfiguration: TLS configuration
     @discardableResult public func addHTTP2Upgrade(tlsConfiguration: TLSConfiguration) throws -> HBHTTPServer {
         var tlsConfiguration = tlsConfiguration
         tlsConfiguration.applicationProtocols.append("h2")
@@ -47,6 +19,8 @@ extension HBHTTPServer {
         return self.addChildChannelHandler(NIOSSLServerHandler(context: sslContext), position: .beforeHTTP)
     }
 
+    /// Set HTTP server to use HTTP2. This assumes that HTTP2 will have already been negotiated. In general you are more likely to use
+    ///`addHTTP2Upgrade` so the HTTP version can be negotiated.
     @discardableResult public func setHTTP2() -> HBHTTPServer {
         self.httpChannelInitializer = HTTP2ChannelInitializer()
         return self
