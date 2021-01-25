e3d65cb6af022fb8b2080405f0b51428ede93210
Adam Fowler <adamfowler71@gmail.com>
2021-01-25 11:14:21 +0000

    Extend Optional and Dictionary to conform to HBResponseEncodable

--- Sources/Hummingbird/Router/ResponseEncodable.swift
+++ Sources/Hummingbird/Router/ResponseEncodable.swift
@@ -1,3 +1,4 @@
+import HummingbirdCore
 
 /// Protocol for encodable object that can generate a response
 public protocol HBResponseEncodable: Encodable, HBResponseGenerator  {}
@@ -12,11 +13,36 @@ extension HBResponseEncodable {
     }
 }
 
+/// Extend Optional to conform to HBResponseGenerator
+extension Optional: HBResponseGenerator where Wrapped: HBResponseEncodable {}
+
+/// Extend Optional to conform to HBResponseEncodable
+extension Optional: HBResponseEncodable where Wrapped: HBResponseEncodable {
+    public func response(from request: HBRequest) throws -> HBResponse {
+        switch self {
+        case .some(let wrapped):
+            return try request.application.encoder.encode(wrapped, from: request)
+        case .none:
+            throw HBHTTPError(.notFound)
+        }
+    }
+}
+
 /// Extend Array to conform to HBResponseGenerator
 extension Array: HBResponseGenerator where Element: HBResponseEncodable {}
 
 /// Extend Array to conform to HBResponseEncodable
 extension Array: HBResponseEncodable where Element: HBResponseEncodable {
+    public func response(from request: HBRequest) throws -> HBResponse {
+        return try request.application.encoder.encode(self, from: request)
+    }
+}
+
+/// Extend Dictionary to conform to HBResponseGenerator
+extension Dictionary: HBResponseGenerator where Key: HBResponseEncodable, Value: HBResponseEncodable {}
+
+/// Extend Array to conform to HBResponseEncodable
+extension Dictionary: HBResponseEncodable where Key: HBResponseEncodable, Value: HBResponseEncodable {
     public func response(from request: HBRequest) throws -> HBResponse {
         return try request.application.encoder.encode(self, from: request)
     }
