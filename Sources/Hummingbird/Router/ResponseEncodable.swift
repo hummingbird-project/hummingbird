27860159fc614ba42612cf8d979546394973d1f1
Adam Fowler <adamfowler71@gmail.com>
2021-01-25 10:47:14 +0000

    Add HBResponseEncodable conformance to Array

--- Sources/Hummingbird/Router/ResponseEncodable.swift
+++ Sources/Hummingbird/Router/ResponseEncodable.swift
@@ -12,3 +12,13 @@ extension HBResponseEncodable {
     }
 }
 
+/// Extend Array to conform to HBResponseGenerator
+extension Array: HBResponseGenerator where Element: HBResponseEncodable {}
+
+/// Extend Array to conform to HBResponseEncodable
+extension Array: HBResponseEncodable where Element: HBResponseEncodable {
+    public func response(from request: HBRequest) throws -> HBResponse {
+        return try request.application.encoder.encode(self, from: request)
+    }
+}
+
