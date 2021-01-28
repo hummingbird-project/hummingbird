9b246bca4671a4bab41c6ed2af27103cb75c5455
Adam Fowler <adamfowler71@gmail.com>
2021-01-28 10:57:25 +0000

    Add head and patch router methods

--- Sources/Hummingbird/Router/RouterMethods.swift
+++ Sources/Hummingbird/Router/RouterMethods.swift
@@ -24,9 +24,19 @@ extension HBRouterMethods {
         return on(path, method: .POST, use: closure)
     }
 
+    /// HEAD path for closure returning type conforming to ResponseFutureEncodable
+    @discardableResult public func head<R: HBResponseGenerator>(_ path: String = "", use closure: @escaping (HBRequest) throws -> R) -> Self {
+        return on(path, method: .HEAD, use: closure)
+    }
+
     /// DELETE path for closure returning type conforming to ResponseFutureEncodable
     @discardableResult public func delete<R: HBResponseGenerator>(_ path: String = "", use closure: @escaping (HBRequest) throws -> R) -> Self {
         return on(path, method: .DELETE, use: closure)
+    }
+
+    /// PATCH path for closure returning type conforming to ResponseFutureEncodable
+    @discardableResult public func patch<R: HBResponseGenerator>(_ path: String = "", use closure: @escaping (HBRequest) throws -> R) -> Self {
+        return on(path, method: .PATCH, use: closure)
     }
 
     /// GET path for closure returning type conforming to ResponseFutureEncodable
@@ -39,13 +49,23 @@ extension HBRouterMethods {
         return on(path, method: .PUT, use: closure)
     }
 
+    /// POST path for closure returning type conforming to ResponseFutureEncodable
+    @discardableResult public func delete<R: HBResponseFutureGenerator>(_ path: String = "", use closure: @escaping (HBRequest) -> R) -> Self {
+        return on(path, method: .DELETE, use: closure)
+    }
+
+    /// HEAD path for closure returning type conforming to ResponseFutureEncodable
+    @discardableResult public func head<R: HBResponseFutureGenerator>(_ path: String = "", use closure: @escaping (HBRequest) -> R) -> Self {
+        return on(path, method: .HEAD, use: closure)
+    }
+
     /// DELETE path for closure returning type conforming to ResponseFutureEncodable
     @discardableResult public func post<R: HBResponseFutureGenerator>(_ path: String = "", use closure: @escaping (HBRequest) -> R) -> Self {
         return on(path, method: .POST, use: closure)
     }
 
-    /// POST path for closure returning type conforming to ResponseFutureEncodable
-    @discardableResult public func delete<R: HBResponseFutureGenerator>(_ path: String = "", use closure: @escaping (HBRequest) -> R) -> Self {
-        return on(path, method: .DELETE, use: closure)
+    /// PATCH path for closure returning type conforming to ResponseFutureEncodable
+    @discardableResult public func patch<R: HBResponseFutureGenerator>(_ path: String = "", use closure: @escaping (HBRequest) -> R) -> Self {
+        return on(path, method: .PATCH, use: closure)
     }
 }
