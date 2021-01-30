4f1a9ed6f353f15cbcb07b9354daf9c4370a4615
Adam Fowler <adamfowler71@gmail.com>
2021-01-30 17:25:46 +0000

    Extend EventLoopStorageMap to conform to Sequence

--- Sources/Hummingbird/Extensions/EventLoopStorage.swift
+++ Sources/Hummingbird/Extensions/EventLoopStorage.swift
@@ -12,26 +12,28 @@ extension HBApplication {
         }
     }
 
-    struct EventLoopStorageMap {
+    public struct EventLoopStorageMap {
         init(eventLoopGroup: EventLoopGroup) {
             var eventLoops: [EventLoop.Key: EventLoopStorage] = [:]
             for eventLoop in eventLoopGroup.makeIterator() {
                 eventLoops[eventLoop.key] = .init()
             }
             self.eventLoops = eventLoops
+            self.eventLoopGroup = eventLoopGroup
         }
 
-        func get(for eventLoop: EventLoop) -> EventLoopStorage {
+        public func get(for eventLoop: EventLoop) -> EventLoopStorage {
             guard let storage = eventLoops[eventLoop.key] else {
                 preconditionFailure("EventLoop must be from the Application's EventLoopGroup")
             }
             return storage
         }
 
+        fileprivate let eventLoopGroup: EventLoopGroup
         fileprivate let eventLoops: [EventLoop.Key: EventLoopStorage]
     }
 
-    var eventLoopStorage: EventLoopStorageMap {
+    public var eventLoopStorage: EventLoopStorageMap {
         get { return extensions.get(\.eventLoopStorage) }
         set { return extensions.set(\.eventLoopStorage, value: newValue) }
     }
@@ -56,5 +58,36 @@ private extension EventLoop {
     typealias Key = ObjectIdentifier
     var key: Key {
         ObjectIdentifier(self)
+    }
+}
+
+/// extend EventLoopStorageMap to be a Sequence
+extension HBApplication.EventLoopStorageMap: Sequence {
+    /// EventLoopStorageMap iterator
+    public struct Iterator: Sequence, IteratorProtocol {
+        public typealias Element = (eventLoop: EventLoop, storage: HBApplication.EventLoopStorage)
+        
+        private var eventLoopStorage: HBApplication.EventLoopStorageMap
+        private var iterator: EventLoopIterator
+
+        /// Create an `Iterator` from an array of `EventLoop`s.
+        public init(_ eventLoopStorage: HBApplication.EventLoopStorageMap) {
+            self.eventLoopStorage = eventLoopStorage
+            self.iterator = eventLoopStorage.eventLoopGroup.makeIterator()
+        }
+
+        /// Advances to the next `EventLoop` and returns it, or `nil` if no next element exists.
+        ///
+        /// - returns: The next `EventLoop` if a next element exists; otherwise, `nil`.
+        public mutating func next() -> Element? {
+            guard let eventLoop = self.iterator.next() else { return nil }
+            return (eventLoop: eventLoop, storage: eventLoopStorage.get(for: eventLoop))
+        }
+    }
+    
+    public typealias Element = Iterator.Element
+    
+    public func makeIterator() -> HBApplication.EventLoopStorageMap.Iterator {
+        return Iterator(self)
     }
 }
