13038b513220a7fdb0485d2e5f995f126c5777d8
Adam Fowler <adamfowler71@gmail.com>
2021-02-05 16:36:23 +0000

    Use swift-nio 2.21.0
    Needed for NIOEventLoopGroupProvider

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.16.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.21.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.16.1"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.4.0"),
