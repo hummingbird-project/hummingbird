9b65a891788347e9e159ea30b88fc4dad2e90b27
Adam Fowler <adamfowler71@gmail.com>
2021-02-07 23:09:20 +0000

    Comments for HBParameters

--- Sources/Hummingbird/Router/Parameters.swift
+++ Sources/Hummingbird/Router/Parameters.swift
@@ -1,22 +1,33 @@
-/// parameter map, extracted from Request URI
+/// Store for parameters extracted from Request URI
 public struct HBParameters {
     var parameters: [Substring: Substring]
 
     init() {
         self.parameters = [:]
     }
-
+    
+    /// Return parameter with specified id
+    /// - Parameter s: parameter id
     public func get(_ s: Substring) -> Substring? {
         return self.parameters[s[...]]
     }
-
+    
+    /// Return parameter with specified id as a certain type
+    /// - Parameters:
+    ///   - s: parameter id
+    ///   - as: type we want returned
     public func get<T: LosslessStringConvertible>(_ s: Substring, as: T.Type) -> T? {
         return self.parameters[s[...]].map { T(String($0)) } ?? nil
     }
-
+    
+    /// Set parameter
+    /// - Parameters:
+    ///   - s: parameter id
+    ///   - value: parameter value
     public mutating func set(_ s: Substring, value: Substring) {
         self.parameters[s] = value
     }
 
+    /// number of parameters
     public var count: Int { return self.parameters.count }
 }
