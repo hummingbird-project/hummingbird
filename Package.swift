938d04f97c9828b4e32d0225dfe95b503a14b9c9
Adam Fowler <adamfowler71@gmail.com>
2021-02-07 20:44:13 +0000

    Add HBMetricsMiddleware

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.21.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.7.0"),
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.16.1"),
@@ -28,6 +29,7 @@ let package = Package(
             .product(name: "Lifecycle", package: "swift-service-lifecycle"),
             .product(name: "LifecycleNIOCompat", package: "swift-service-lifecycle"),
             .product(name: "Logging", package: "swift-log"),
+            .product(name: "Metrics", package: "swift-metrics"),
             .product(name: "NIO", package: "swift-nio"),
             .product(name: "NIOHTTP1", package: "swift-nio"),
         ]),
---
+++ Sources/Hummingbird/Middleware/MetricsMiddleware.swift
@@ -0,0 +1,31 @@
+import Dispatch
+import Metrics
+
+/// Middleware recording metrics for each request
+///
+/// Records the number of requests, the request duration and how many errors were thrown. Each metric has additional
+/// dimensions URI and method.
+public struct HBMetricsMiddleware: HBMiddleware {
+    public func apply(to request: HBRequest, next: HBResponder) -> EventLoopFuture<HBResponse> {
+        let dimensions: [(String, String)] = [
+            ("hb_uri", request.uri.description),
+            ("hb_method", request.method.rawValue)
+        ]
+        let startTime = DispatchTime.now().uptimeNanoseconds
+
+        Counter(label: "hb_requests", dimensions: dimensions).increment()
+
+        return next.respond(to: request).map { response in
+            Metrics.Timer(
+                label: "hb_request_duration",
+                dimensions: dimensions,
+                preferredDisplayUnit: .seconds
+            ).recordNanoseconds(DispatchTime.now().uptimeNanoseconds - startTime)
+            return response
+        }
+        .flatMapErrorThrowing { error in
+            Counter(label: "hb_errors", dimensions: dimensions).increment()
+            throw error
+        }
+    }
+}
