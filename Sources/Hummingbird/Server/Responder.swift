8b19f2486f36fdf6f301a42c92529703779bf5d4
Adam Fowler <adamfowler71@gmail.com>
2021-02-08 10:48:29 +0000

    Responder comments

--- Sources/Hummingbird/Server/Responder.swift
+++ Sources/Hummingbird/Server/Responder.swift
@@ -1,13 +1,17 @@
 import NIO
 
-/// Object that produces a response given a request
+/// Protocol for object that produces a response given a request
+///
+/// This is the core protocol for Hummingbird. It defines an object that can respond to a request.
 public protocol HBResponder {
+    /// Return EventLoopFuture that will be fulfilled with response to the request supplied
     func respond(to request: HBRequest) -> EventLoopFuture<HBResponse>
 }
 
 /// Responder that calls supplied closure
 struct CallbackResponder: HBResponder {
     let callback: (HBRequest) -> EventLoopFuture<HBResponse>
+    /// Return EventLoopFuture that will be fulfilled with response to the request supplied
     func respond(to request: HBRequest) -> EventLoopFuture<HBResponse> {
         return self.callback(request)
     }
