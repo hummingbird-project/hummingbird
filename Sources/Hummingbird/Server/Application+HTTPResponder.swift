3b2114188f6dfd6a8490cbe54042acbccb305876
Adam Fowler <adamfowler71@gmail.com>
2021-02-08 10:04:32 +0000

    HTTPResponder comments

--- Sources/Hummingbird/Server/Application+HTTPResponder.swift
+++ Sources/Hummingbird/Server/Application+HTTPResponder.swift
@@ -4,18 +4,29 @@ import NIO
 import NIOHTTP1
 
 extension HBApplication {
+    /// HTTP responder class for Hummingbird. This is the interface between Hummingbird and HummingbirdCore
+    ///
+    /// The HummingbirdCore server calls `respond` to get the HTTP response from Hummingbird
     public struct HTTPResponder: HBHTTPResponder {
         let application: HBApplication
         let responder: HBResponder
-
+        
+        /// Construct HTTP responder
+        /// - Parameter application: application creating this responder
         public init(application: HBApplication) {
             self.application = application
             // application responder has been set for sure
             self.responder = application.constructResponder()
         }
-
+        
+        /// Logger used by responder
         public var logger: Logger? { return self.application.logger }
-
+        
+        /// Return EventLoopFuture that will be fulfilled with the HTTP response for the supplied HTTP request
+        /// - Parameters:
+        ///   - request: request
+        ///   - context: context from ChannelHandler
+        /// - Returns: response
         public func respond(to request: HBHTTPRequest, context: ChannelHandlerContext) -> EventLoopFuture<HBHTTPResponse> {
             let request = HBRequest(
                 head: request.head,
