a7c4691aa851fcce8be87ff053b5cc06f2555957
Adam Fowler <adamfowler71@gmail.com>
2021-02-15 14:58:40 +0000

    Use hummingbird-core 0.4.2

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.4.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.2.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.6"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", from: "0.3.0"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", from: "0.4.2"),
     ],
     targets: [
         .target(name: "CURLParser", dependencies: []),
