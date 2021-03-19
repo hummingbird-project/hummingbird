e236e72a99511cdc65b48e4df8ea925d4dc57c91
Adam Fowler <adamfowler71@gmail.com>
2021-03-19 11:02:07 +0000

    Create EventLoopFuture+async.swift

---
+++ Sources/Hummingbird/EventLoopFuture+async.swift
@@ -0,0 +1,16 @@
+import NIO
+
+extension EventLoopFuture {
+    public func get() async throws -> Value {
+        return try await withUnsafeThrowingContinuation { cont in
+            self.whenComplete { result in
+                switch result {
+                case .success(let value):
+                    cont.resume(returning: value)
+                case .failure(let error):
+                    cont.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
