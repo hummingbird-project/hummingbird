0fe4e018947e567f807460da63df02adaf85a088
Adam Fowler <adamfowler71@gmail.com>
2021-04-02 12:35:41 +0100

    Add HBParameters.require

--- Sources/Hummingbird/Router/Parameters.swift
+++ Sources/Hummingbird/Router/Parameters.swift
@@ -20,6 +20,27 @@ public struct HBParameters {
         return self.parameters[s[...]].map { T(String($0)) } ?? nil
     }
 
+    /// Return parameter with specified id
+    /// - Parameter s: parameter id
+    public func require(_ s: String) throws -> String {
+        guard let param = self.parameters[s[...]].map({ String($0) }) else {
+            throw HBHTTPError(.badRequest)
+        }
+        return param
+    }
+
+    /// Return parameter with specified id as a certain type
+    /// - Parameters:
+    ///   - s: parameter id
+    ///   - as: type we want returned
+    public func require<T: LosslessStringConvertible>(_ s: String, as: T.Type) throws -> T {
+        guard let param = self.parameters[s[...]],
+              let result = T(String(param)) else {
+            throw HBHTTPError(.badRequest)
+        }
+        return result
+    }
+
     /// Set parameter
     /// - Parameters:
     ///   - s: parameter id
