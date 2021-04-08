a92d56574a530d90833fc04bf15861907ff34a95
Adam Fowler <adamfowler71@gmail.com>
2021-04-08 17:20:30 +0100

    Add file header using swift format

--- Sources/HummingbirdCore/Server/HTTPServer.swift
+++ Sources/HummingbirdCore/Server/HTTPServer.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2021 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import NIO
 import NIOExtras
 import NIOHTTP1
