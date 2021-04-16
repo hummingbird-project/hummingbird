97632cd2279a3006ed90466530940383085ec748
Adam Fowler <adamfowler71@gmail.com>
2021-04-16 15:01:34 +0100

    Fix DateCache so it works with NIOTSEventLoopGroup
    Will need this fix for future async await code

--- Sources/Hummingbird/Application.swift
+++ Sources/Hummingbird/Application.swift
@@ -100,8 +100,6 @@ public final class HBApplication: HBExtensible {
 
         self.addEventLoopStorage()
 
-        HBDateCache.initDateCaches(for: self.eventLoopGroup)
-
         // register application shutdown with lifecycle
         self.lifecycle.registerShutdown(
             label: "Application", .sync(self.shutdownApplication)
@@ -149,7 +147,6 @@ public final class HBApplication: HBExtensible {
 
     /// shutdown eventloop, threadpool and any extensions attached to the Application
     public func shutdownApplication() throws {
-        HBDateCache.shutdownDateCaches(for: self.eventLoopGroup)
         try self.extensions.shutdown()
         try self.threadPool.syncShutdownGracefully()
         if case .createNew = self.eventLoopGroupProvider {
