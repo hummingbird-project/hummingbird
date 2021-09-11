5990e7ebf762397b03bb531329ba278a0c231ee2
Adam Fowler <adamfowler71@gmail.com>
2021-09-11 17:28:23 +0100

    `HBRequest.cookies` returns a temporary object instead of modifying `HBRequest` (#95)
    * HBRequest.cookies is a temporary value
    
    Getting HBRequest.cookies no longer modifies HBRequest
    
    * Now we don't use `HBExtensions.getOrCreate` lets remove it

--- Sources/HummingbirdFoundation/Cookies/Cookies.swift
+++ Sources/HummingbirdFoundation/Cookies/Cookies.swift
@@ -18,41 +18,24 @@ import Hummingbird
 ///
 /// Cookies can be accessed from request via `HBRequest.cookies`.
 public struct HBCookies {
-    public typealias CollectionType = [String: HBCookie]
-
-    /// Construct array of cookies from `HBRequest`
+    /// Construct cookies accessor from `HBRequest`
     /// - Parameter request: request to get cookies from
     init(from request: HBRequest) {
-        self.map = .init(request.headers["cookie"].flatMap {
-            return $0.split(separator: ";")
-                .compactMap {
-                    guard let cookie = HBCookie(from: String($0.drop { $0.isWhitespace })) else { return nil }
-                    return (cookie.name, cookie)
-                }
-        }) { first, _ in first }
+        self.cookieStrings = request.headers["cookie"].flatMap {
+            return $0.split(separator: ";").map { $0.drop { $0.isWhitespace } }
+        }
     }
 
     /// access cookies via dictionary subscript
     public subscript(_ key: String) -> HBCookie? {
-        get { return self.map[key] }
-        set { self.map[key] = newValue }
+        guard let cookieString = cookieStrings.first(where: {
+            guard let cookieName = HBCookie.getName(from: $0) else { return false }
+            return cookieName == key
+        }) else {
+            return nil
+        }
+        return HBCookie(from: cookieString)
     }
 
-    var map: CollectionType
-}
-
-/// extend `HBCookies` to conform to `Collection`
-extension HBCookies: Collection {
-    public typealias Element = CollectionType.Element
-
-    public func index(after i: CollectionType.Index) -> CollectionType.Index {
-        return self.map.index(after: i)
-    }
-
-    public subscript(_ index: CollectionType.Index) -> HBCookies.Element {
-        return self.map[index]
-    }
-
-    public var startIndex: CollectionType.Index { self.map.startIndex }
-    public var endIndex: CollectionType.Index { self.map.endIndex }
+    var cookieStrings: [Substring]
 }
