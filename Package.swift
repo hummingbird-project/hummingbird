f955bde558051146413ea5996c278faa83dae98d
Adam Fowler <adamfowler71@gmail.com>
2021-09-24 07:40:03 +0100

    Remove _NIOConcurrency imports, use SwiftNIO 2.33.0

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .library(name: "Hummingbird", targets: ["Hummingbird"]),
         .library(name: "HummingbirdFoundation", targets: ["HummingbirdFoundation"]),
         .library(name: "HummingbirdXCT", targets: ["HummingbirdXCT"]),
+        .executable(name: "PerformanceTest", targets: ["PerformanceTest"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.32.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.4.0"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.6"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", .upToNextMinor(from: "0.12.0")),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.33.0"),
+        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.9"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", .upToNextMinor(from: "0.12.1")),
     ],
     targets: [
         .target(name: "Hummingbird", dependencies: [
@@ -29,7 +29,6 @@ let package = Package(
             .product(name: "NIOCore", package: "swift-nio"),
             .product(name: "NIOPosix", package: "swift-nio"),
             .product(name: "NIOHTTP1", package: "swift-nio"),
-            .product(name: "_NIOConcurrency", package: "swift-nio"),
         ]),
         .target(name: "HummingbirdFoundation", dependencies: [
             .byName(name: "Hummingbird"),
--- Sources/Hummingbird/AsyncAwaitSupport/AsyncMiddleware.swift
+++ Sources/Hummingbird/AsyncAwaitSupport/AsyncMiddleware.swift
@@ -14,7 +14,7 @@
 
 #if compiler(>=5.5) && canImport(_Concurrency)
 
-import _NIOConcurrency
+import NIOCore
 
 /// Middleware using async/await
 @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
