0b9ae826ef769e60938d5be2f27e25f8d29decea
Adam Fowler <adamfowler71@gmail.com>
2021-10-22 12:39:14 +0100

    Add Async await support for jobs (#105)

---
+++ Sources/HummingbirdJobs/AsyncAwaitSupport/AsyncJob.swift
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2021 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+
+import NIOCore
+
+/// Job with asynchronous handler
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+public protocol HBAsyncJob: HBJob {
+    /// Execute job
+    func execute(logger: Logger) async throws
+}
+
+extension HBAsyncJob {
+    func execute(on eventLoop: EventLoop, logger: Logger) -> EventLoopFuture<Void> {
+        let promise = eventLoop.makePromise(of: Void.self)
+        promise.completeWithTask {
+            try await execute(logger: logger)
+        }
+        return promise.futureResult
+    }
+}
+
+#endif // compiler(>=5.5) && canImport(_Concurrency)
