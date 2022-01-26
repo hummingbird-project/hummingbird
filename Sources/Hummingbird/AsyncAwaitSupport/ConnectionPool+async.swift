0dfcf6b189a2b1287127808af0bf280f2087f032
Adam Fowler <adamfowler71@gmail.com>
2022-01-26 17:42:41 +0100

    Connection pool (#116)
    * Add ConnectionPool class
    * Use switch in _release
    * Improve logging
    * swift format
    * Fix testCheckCloseFlag
    * Add HBConnectionPoolGroup
    * Add HBConnectionPool.lease()
    * Add wrapper functions to HBConnectionPoolGroup
    * Add `HBConnectionSource`
    * Add async functions for connection pool code
    * Update headers
    * Remove logger parameter from HBConnection.close()
    * Update dates in headers

---
+++ Sources/Hummingbird/AsyncAwaitSupport/ConnectionPool+async.swift
@@ -0,0 +1,83 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2022 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#if compiler(>=5.5) && canImport(_Concurrency)
+
+import Logging
+import NIO
+
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+extension HBConnectionPool {
+    /// Request a connection, run a process and then release the connection
+    /// - Parameters:
+    ///   -logger: Logger used for logging
+    ///   - process: Closure to run while we have the connection
+    public func lease<NewValue>(logger: Logger, process: @escaping (Source.Connection) async throws -> NewValue) async throws -> NewValue {
+        return try await self.lease(logger: logger) { connection in
+            let promise = self.eventLoop.makePromise(of: NewValue.self)
+            promise.completeWithTask {
+                return try await process(connection)
+            }
+            return promise.futureResult
+        }.get()
+    }
+
+    /// Request a connection
+    /// - Parameter logger: Logger used for logging
+    /// - Returns: Returns a connection when available
+    public func request(logger: Logger) async throws -> Source.Connection {
+        return try await self.request(logger: logger).get()
+    }
+
+    /// Close connection pool
+    /// - Parameter logger: Logger used for logging
+    /// - Returns: Returns when close is complete
+    public func close(logger: Logger) async throws {
+        return try await self.close(logger: logger).get()
+    }
+}
+
+@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+extension HBConnectionPoolGroup {
+    /// Request a connection, run a process and then release the connection
+    /// - Parameters:
+    ///   - eventLoop: event loop to find associated connection pool
+    ///   -logger: Logger used for logging
+    ///   - process: Closure to run while we have the connection
+    public func lease<NewValue>(
+        on eventLoop: EventLoop,
+        logger: Logger,
+        process: @escaping (Source.Connection) async throws -> NewValue
+    ) async throws -> NewValue {
+        let pool = self.getConnectionPool(on: eventLoop)
+        return try await pool.lease(logger: logger, process: process)
+    }
+
+    /// Request a connection
+    /// - Parameters:
+    ///   - eventLoop: event loop to find associated connection pool
+    ///   - logger: Logger used for logging
+    /// - Returns: Returns a connection when available
+    public func request(on eventLoop: EventLoop, logger: Logger) async throws -> Source.Connection {
+        let pool = self.getConnectionPool(on: eventLoop)
+        return try await pool.request(logger: logger)
+    }
+
+    /// Close connection pool group
+    public func close() async throws {
+        return try await self.close().get()
+    }
+}
+
+#endif // compiler(>=5.5) && canImport(_Concurrency)
---
+++ Sources/Hummingbird/ConnectionPool/ConnectionPoolGroup.swift
@@ -0,0 +1,86 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2022 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Logging
+import NIOCore
+
+/// Array of connection pools associated with an EventLoopGroup.
+///
+/// Each EventLoop has a connection pool associated with it
+public class HBConnectionPoolGroup<Source: HBConnectionSource> {
+    let pools: [EventLoop.Key: HBConnectionPool<Source>]
+    let eventLoopGroup: EventLoopGroup
+    let logger: Logger
+
+    /// Create `HBConnectionPoolGroup`
+    ///
+    /// - Parameters:
+    ///     - maxConnections: Maximum connections each EventLoop can make
+    ///     - eventLoopGroup: `EventLoopGroup`` associated with this `HBConnectionPoolGroup`
+    public init(source: Source, maxConnections: Int, eventLoopGroup: EventLoopGroup, logger: Logger) {
+        var pools: [EventLoop.Key: HBConnectionPool<Source>] = [:]
+        for eventLoop in eventLoopGroup.makeIterator() {
+            pools[eventLoop.key] = HBConnectionPool(source: source, maxConnections: maxConnections, eventLoop: eventLoop)
+        }
+        self.eventLoopGroup = eventLoopGroup
+        self.logger = logger
+        self.pools = pools
+    }
+
+    /// Request a connection, run a process and then release the connection
+    /// - Parameters:
+    ///   - eventLoop: event loop to find associated connection pool
+    ///   -logger: Logger used for logging
+    ///   - process: Closure to run while we have the connection
+    public func lease<NewValue>(
+        on eventLoop: EventLoop,
+        logger: Logger,
+        process: @escaping (Source.Connection) -> EventLoopFuture<NewValue>
+    ) -> EventLoopFuture<NewValue> {
+        let pool = self.getConnectionPool(on: eventLoop)
+        return pool.lease(logger: logger, process: process)
+    }
+
+    /// Request a connection
+    /// - Parameters:
+    ///   - eventLoop: event loop to find associated connection pool
+    ///   - logger: Logger used for logging
+    /// - Returns: Returns a connection when available
+    public func request(on eventLoop: EventLoop, logger: Logger) -> EventLoopFuture<Source.Connection> {
+        let pool = self.getConnectionPool(on: eventLoop)
+        return pool.request(logger: logger)
+    }
+
+    /// Release a connection back onto the pool
+    /// - Parameters:
+    ///   - eventLoop: event loop to find associated connection pool
+    ///   - logger: Logger used for logging
+    public func release(connection: Source.Connection, on eventLoop: EventLoop, logger: Logger) {
+        let pool = self.getConnectionPool(on: eventLoop)
+        pool.release(connection: connection, logger: logger)
+    }
+
+    /// Return Connection Pool associated with EventLoopGroup
+    public func getConnectionPool(on eventLoop: EventLoop) -> HBConnectionPool<Source> {
+        let pool = self.pools[eventLoop.key]
+        assert(pool != nil, "No connection pool available for event loop")
+        return pool!
+    }
+
+    /// Close connection pool group
+    public func close() -> EventLoopFuture<Void> {
+        let closeFutures: [EventLoopFuture<Void>] = self.pools.values.map { $0.close(logger: self.logger) }
+        return EventLoopFuture.andAllComplete(closeFutures, on: self.eventLoopGroup.any())
+    }
+}
