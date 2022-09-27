c0730cd0e859aa2c6aabe015d87b70324247bdf9
Moritz Lang <hi@slashmo.codes>
2022-09-27 22:18:47 +0200

    Add ServiceLifecycleProvider

---
+++ Sources/Hummingbird/Lifecycle/ServiceLifecycleProvider.swift
@@ -0,0 +1,24 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2021 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Lifecycle
+
+public enum ServiceLifecycleProvider {
+    /// Use a `ServiceLifecycle` provided by the user
+    /// and run `HBApplication` tasks in a `ComponentLifecycle`.
+    case shared(ServiceLifecycle)
+
+    /// Create a new `ServiceLifecycle`.
+    case createNew
+}
