b3c6078ff51d2923c3f64d17d1997b7516a18793
Adam Fowler <adamfowler71@gmail.com>
2022-10-31 09:53:55 +0000

    Make PerformanceTest an executableTarget

--- Sources/Hummingbird/Utils/FlatDictionary.swift
+++ Sources/Hummingbird/Utils/FlatDictionary.swift
@@ -118,5 +118,7 @@ public struct FlatDictionary<Key: Hashable, Value>: Collection {
     private var hashKeys: [Int]
 }
 
+#if compiler(>=5.6)
 // FlatDictionary is Sendable when Key and Value are Sendable
 extension FlatDictionary: Sendable where Key: Sendable, Value: Sendable {}
+#endif
