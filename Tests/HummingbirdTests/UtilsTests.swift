144841342dcc950471ff277cb1c296ae2c9e3f6f
Adam Fowler <adamfowler71@gmail.com>
2023-01-04 08:03:12 +0000

    Add tests for flat dictionary, fix dictionaryLiteral

---
+++ Tests/HummingbirdTests/UtilsTests.swift
@@ -0,0 +1,39 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2023 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Hummingbird
+import XCTest
+
+class FlatDictionaryTests: XCTestCase {
+    func testLiteralInit() {
+        let a: FlatDictionary<String, String> = ["test": "value", "key2": "value2"]
+        XCTAssertEqual(a["test"], "value")
+        XCTAssertEqual(a["key2"], "value2")
+    }
+
+    func testKeyGetSet() {
+        var a: FlatDictionary<String, String> = [:]
+        a["key"] = "value"
+        XCTAssertEqual(a["key"], "value")
+        a["key"] = nil
+        XCTAssertEqual(a["key2"], nil)
+    }
+
+    func testKeyGetFirst() {
+        var a: FlatDictionary<String, String> = [:]
+        a.append(key: "key", value: "value1")
+        a.append(key: "key", value: "value2")
+        XCTAssertEqual(a["key"], "value1")
+    }
+}
