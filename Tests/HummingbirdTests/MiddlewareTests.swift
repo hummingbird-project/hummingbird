cd88961378c0bc139de1c46a9e4c1d022155c80a
Moritz Lang <hi@slashmo.codes>
2023-01-08 13:20:57 +0100

    Add HBTracingMiddleware

--- Tests/HummingbirdTests/MiddlewareTests.swift
+++ Tests/HummingbirdTests/MiddlewareTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Hummingbird server framework project
 //
-// Copyright (c) 2021-2021 the Hummingbird authors
+// Copyright (c) 2021-2023 the Hummingbird authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -14,6 +14,8 @@
 
 @testable import Hummingbird
 import HummingbirdXCT
+@testable import Instrumentation
+import Tracing
 import XCTest
 
 final class MiddlewareTests: XCTestCase {
@@ -203,5 +205,303 @@ final class MiddlewareTests: XCTestCase {
 
         try app.XCTExecute(uri: "/hello", method: .PUT) { _ in
         }
+    }
+
+    func testTracingMiddleware() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware())
+        app.router.get("users/:id") { _ -> String in
+            return "42"
+        }
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        try app.XCTExecute(uri: "/users/42", method: .GET) { response in
+            XCTAssertEqual(response.status, .ok)
+            var responseBody = try XCTUnwrap(response.body)
+            XCTAssertEqual(responseBody.readString(length: responseBody.readableBytes), "42")
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "/users/:id")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertNil(span.status)
+        XCTAssertTrue(span.errors.isEmpty)
+
+        XCTAssertEqual(span.attributes.count, 7)
+        XCTAssertEqual(span.attributes["http.method"]?.toSpanAttribute(), "GET")
+        XCTAssertEqual(span.attributes["http.target"]?.toSpanAttribute(), "/users/42")
+        XCTAssertEqual(span.attributes["http.status_code"]?.toSpanAttribute(), 200)
+        XCTAssertEqual(span.attributes["http.response_content_length"]?.toSpanAttribute(), 2)
+        XCTAssertEqual(span.attributes["net.host.name"]?.toSpanAttribute(), "localhost")
+        XCTAssertEqual(span.attributes["net.host.port"]?.toSpanAttribute(), 0)
+        XCTAssertEqual(span.attributes["http.flavor"]?.toSpanAttribute(), "1.1")
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "GET",
+            "http.target": "/users/42",
+            "http.status_code": 200,
+            "http.response_content_length": 2,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+        ])
+    }
+
+    func testTracingMiddlewareServerError() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware())
+        app.router.post("users") { _ -> String in
+            throw HBHTTPError(.internalServerError)
+        }
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        try app.XCTExecute(uri: "/users", method: .POST, body: ByteBuffer(string: "42")) { response in
+            XCTAssertEqual(response.status, .internalServerError)
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "/users")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertEqual(span.status, .init(code: .error))
+
+        XCTAssertEqual(span.errors.count, 1)
+        let error = try XCTUnwrap(span.errors.first as? HBHTTPError, "Recorded unexpected errors: \(span.errors)")
+        XCTAssertEqual(error.status, .internalServerError)
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "POST",
+            "http.target": "/users",
+            "http.status_code": 500,
+            "http.request_content_length": 2,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+        ])
+    }
+
+    func testTracingMiddlewareIncludingHeaders() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware(recordingHeaders: [
+            "accept", "content-type", "cache-control", "does-not-exist",
+        ]))
+        app.router.get("users/:id") { _ -> HBResponse in
+            var headers = HTTPHeaders()
+            headers.add(name: "cache-control", value: "86400")
+            headers.add(name: "cache-control", value: "public")
+            headers.add(name: "content-type", value: "text/plain")
+            return HBResponse(
+                status: .ok,
+                headers: headers,
+                body: .byteBuffer(ByteBuffer(string: "42"))
+            )
+        }
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        var requestHeaders = HTTPHeaders()
+        requestHeaders.add(name: "Accept", value: "text/plain")
+        requestHeaders.add(name: "Accept", value: "application/json")
+        requestHeaders.add(name: "Cache-Control", value: "no-cache")
+        try app.XCTExecute(uri: "/users/42", method: .GET, headers: requestHeaders) { response in
+            XCTAssertEqual(response.status, .ok)
+            var responseBody = try XCTUnwrap(response.body)
+            XCTAssertEqual(responseBody.readString(length: responseBody.readableBytes), "42")
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "/users/:id")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertNil(span.status)
+        XCTAssertTrue(span.errors.isEmpty)
+
+        XCTAssertEqual(span.attributes.count, 11)
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "GET",
+            "http.target": "/users/42",
+            "http.status_code": 200,
+            "http.response_content_length": 2,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+            "http.request.header.accept": .stringArray(["text/plain", "application/json"]),
+            "http.request.header.cache_control": "no-cache",
+            "http.response.header.content_type": "text/plain",
+            "http.response.header.cache_control": .stringArray(["86400", "public"]),
+        ])
+    }
+
+    func testTracingMiddlewareEmptyResponse() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware())
+        app.router.post("/users") { _ -> HTTPResponseStatus in
+            return .noContent
+        }
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        try app.XCTExecute(uri: "/users", method: .POST) { response in
+            XCTAssertEqual(response.status, .noContent)
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "/users")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertNil(span.status)
+        XCTAssertTrue(span.errors.isEmpty)
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "POST",
+            "http.target": "/users",
+            "http.status_code": 204,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+        ])
+    }
+
+    func testTracingMiddlewareIndexRoute() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware())
+        app.router.get("/") { _ -> HTTPResponseStatus in
+            return .ok
+        }
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        try app.XCTExecute(uri: "/", method: .GET) { response in
+            XCTAssertEqual(response.status, .ok)
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "/")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertNil(span.status)
+        XCTAssertTrue(span.errors.isEmpty)
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "GET",
+            "http.target": "/",
+            "http.status_code": 200,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+        ])
+    }
+
+    func testTracingMiddlewareRouteNotFound() throws {
+        let expectation = expectation(description: "Expected span to be ended.")
+
+        let tracer = TestTracer()
+        tracer.onEndSpan = { _ in expectation.fulfill() }
+        InstrumentationSystem.bootstrapInternal(tracer)
+
+        let app = HBApplication(testing: .embedded)
+        app.middleware.add(HBTracingMiddleware())
+        try app.XCTStart()
+        defer { app.XCTStop() }
+
+        try app.XCTExecute(uri: "/", method: .GET) { response in
+            XCTAssertEqual(response.status, .notFound)
+        }
+
+        waitForExpectations(timeout: 1)
+
+        let span = try XCTUnwrap(tracer.spans.first)
+
+        XCTAssertEqual(span.operationName, "HTTP GET route not found")
+        XCTAssertEqual(span.kind, .server)
+        XCTAssertNil(span.status)
+
+        XCTAssertEqual(span.errors.count, 1)
+        let error = try XCTUnwrap(span.errors.first as? HBHTTPError, "Recorded unexpected errors: \(span.errors)")
+        XCTAssertEqual(error.status, .notFound)
+
+        XCTAssertSpanAttributesEqual(span.attributes, [
+            "http.method": "GET",
+            "http.target": "/",
+            "http.status_code": 404,
+            "net.host.name": "localhost",
+            "net.host.port": 0,
+            "http.flavor": "1.1",
+        ])
+    }
+}
+
+private func XCTAssertSpanAttributesEqual(
+    _ lhs: @autoclosure () -> SpanAttributes,
+    _ rhs: @autoclosure () -> [String: SpanAttribute],
+    file: StaticString = #file,
+    line: UInt = #line
+) {
+    var rhs = rhs()
+
+    lhs().forEach { key, attribute in
+        if let rhsValue = rhs.removeValue(forKey: key) {
+            if rhsValue != attribute {
+                XCTFail(
+                    #""\#(key)" was expected to be "\#(rhsValue)" but is actually "\#(attribute)"."#,
+                    file: file,
+                    line: line
+                )
+            }
+        } else {
+            XCTFail(
+                #"Did not specify expected value for "\#(key)", actual value is "\#(attribute)"."#,
+                file: file,
+                line: line
+            )
+        }
+    }
+
+    if !rhs.isEmpty {
+        XCTFail(#"Expected attributes "\#(rhs.keys)" are not present in actual attributes."#, file: file, line: line)
     }
 }
