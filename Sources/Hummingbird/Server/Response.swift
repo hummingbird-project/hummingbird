91256df9fa3f1ab63f34c6fdd8eab989a53b3038
Adam Fowler <adamfowler71@gmail.com>
2023-02-24 09:34:27 +0000

    Add HBResponse.redirect (#174)

--- Sources/Hummingbird/Server/Response.swift
+++ Sources/Hummingbird/Server/Response.swift
@@ -40,6 +40,47 @@ public struct HBResponse: HBSendableExtensible {
     }
 }
 
+extension HBResponse {
+    /// Specifies the type of redirect that the client should receive.
+    public enum RedirectType {
+        /// `301 moved permanently`: The URL of the requested resource has been changed permanently.
+        /// The new URL is iven in the response.
+        case permanent
+        /// `302 found`: This response code means that the URI of requested resource has been changed
+        /// temporarily. Further changes in the URI might be made in the future. Therefore,
+        /// this same URI should be used by the client in future requests.
+        case found
+        /// `303 see other`: The server sent this response to direct the client to get the requested
+        /// resource at another URI with a GET request.
+        case normal
+        /// `307 Temporary`: The server sends this response to direct the client to get the requested
+        /// resource at another URI with the same method that was used in the prior request. This has
+        /// the same semantics as the 302 Found HTTP response code, with the exception that the user
+        /// agent must not change the HTTP method used: if a POST was used in the first request, a POST
+        /// must be used in the second request.
+        case temporary
+
+        /// Associated `HTTPResponseStatus` for this redirect type.
+        public var status: HTTPResponseStatus {
+            switch self {
+            case .permanent: return .movedPermanently
+            case .found: return .found
+            case .normal: return .seeOther
+            case .temporary: return .temporaryRedirect
+            }
+        }
+    }
+
+    ///  Create a redirect response
+    /// - Parameters:
+    ///   - location: Location to redirect to
+    ///   - type: Redirection type
+    /// - Returns: HBResponse with redirection
+    public static func redirect(to location: String, type: RedirectType = .normal) -> HBResponse {
+        return .init(status: type.status, headers: ["location": location])
+    }
+}
+
 extension HBResponse: CustomStringConvertible {
     public var description: String {
         "status: \(self.status), headers: \(self.headers), body: \(self.body)"
