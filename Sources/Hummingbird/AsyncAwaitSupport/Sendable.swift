f59f6a64fdb27ff78912a502069aaeb92c90a60c
Adam Fowler <adamfowler71@gmail.com>
2023-04-03 18:04:57 +0100

    Add _HBSendableProtocol back in

---
+++ Sources/Hummingbird/AsyncAwaitSupport/Sendable.swift
@@ -0,0 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2023 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@available(*, deprecated, message: "Use Sendable instead of _HBSendableProtocol")
+@preconcurrency public protocol _HBSendableProtocol: Sendable {}
