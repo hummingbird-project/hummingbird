1802a96b6c9952636635addafcf56fe5ee12fc66
Adam Fowler <adamfowler71@gmail.com>
2023-11-28 10:49:06 +0000

    Cleanup HBApplicationConfiguration
    Remove unused values, ensure other values are being used correctly

--- Sources/Hummingbird/Configuration.swift
+++ Sources/Hummingbird/Configuration.swift
@@ -32,12 +32,6 @@ public struct HBApplicationConfiguration: Sendable {
     /// Maximum upload size allowed for routes that don't stream the request payload. This
     /// limits how much memory would be used for one request
     public let maxUploadSize: Int
-    /// Maximum upload size allowed when streaming. This value is passed down to the server
-    /// as at the server everything is considered to be streamed. This limits how much data
-    /// will be passed through the HTTP channel
-    public let maxStreamedUploadSize: Int
-    /// Maximum size of data in flight while streaming request payloads before back pressure is applied.
-    public let maxStreamingBufferSize: Int
     /// Defines the maximum length for the queue of pending connections
     public let backlog: Int
     /// Allows socket to be bound to an address that is already in use.
@@ -47,8 +41,6 @@ public struct HBApplicationConfiguration: Sendable {
     public let tlsOptions: TSTLSOptions
     #endif
 
-    /// Number of threads to allocate in the application thread pool
-    public let threadPoolSize: Int
     /// don't run the HTTP server
     public let noHTTPServer: Bool
     /// logging level
@@ -62,20 +54,15 @@ public struct HBApplicationConfiguration: Sendable {
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
     ///   - maxUploadSize: Maximum upload size allowed for routes that don't stream the request payload
-    ///   - maxStreamedUploadSize: Maximum upload size allowed when streaming data
-    ///   - maxStreamingBufferSize: Maximum size of data in flight while streaming request payloads before back pressure is applied.
     ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
     ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - threadPoolSize: Number of threads in application thread pool
     ///   - logLevel: Logging level
     ///   - noHTTPServer: Don't start up the HTTP server.
     public init(
         address: HBBindAddress = .hostname(),
         serverName: String? = nil,
         maxUploadSize: Int = 1 * 1024 * 1024,
-        maxStreamedUploadSize: Int = 4 * 1024 * 1024,
-        maxStreamingBufferSize: Int = 1 * 1024 * 1024,
         backlog: Int = 256,
         reuseAddress: Bool = true,
         threadPoolSize: Int = 2,
@@ -87,18 +74,15 @@ public struct HBApplicationConfiguration: Sendable {
         self.address = address
         self.serverName = serverName
         self.maxUploadSize = maxUploadSize
-        self.maxStreamedUploadSize = maxStreamedUploadSize
-        self.maxStreamingBufferSize = maxStreamingBufferSize
         self.backlog = backlog
         self.reuseAddress = reuseAddress
         #if canImport(Network)
         self.tlsOptions = .none
         #endif
 
-        self.threadPoolSize = threadPoolSize
         self.noHTTPServer = noHTTPServer
 
-        if let logLevel = logLevel {
+        if let logLevel {
             self.logLevel = logLevel
         } else if let logLevel = env.get("LOG_LEVEL") {
             self.logLevel = Logger.Level(rawValue: logLevel) ?? .info
@@ -114,9 +98,7 @@ public struct HBApplicationConfiguration: Sendable {
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
     ///   - maxUploadSize: Maximum upload size allowed for routes that don't stream the request payload
-    ///   - maxStreamingBufferSize: Maximum size of data in flight while streaming request payloads before back pressure is applied.
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - threadPoolSize: Number of threads in application thread pool
     ///   - logLevel: Logging level
     ///   - noHTTPServer: Don't start up the HTTP server.
     ///   - tlsOptions: TLS options for when you are using NIOTransportServices
@@ -124,10 +106,7 @@ public struct HBApplicationConfiguration: Sendable {
         address: HBBindAddress = .hostname(),
         serverName: String? = nil,
         maxUploadSize: Int = 1 * 1024 * 1024,
-        maxStreamedUploadSize: Int = 4 * 1024 * 1024,
-        maxStreamingBufferSize: Int = 1 * 1024 * 1024,
         reuseAddress: Bool = true,
-        threadPoolSize: Int = 2,
         logLevel: Logger.Level? = nil,
         noHTTPServer: Bool = false,
         tlsOptions: TSTLSOptions
@@ -137,16 +116,13 @@ public struct HBApplicationConfiguration: Sendable {
         self.address = address
         self.serverName = serverName
         self.maxUploadSize = maxUploadSize
-        self.maxStreamedUploadSize = maxStreamedUploadSize
-        self.maxStreamingBufferSize = maxStreamingBufferSize
         self.backlog = 256 // not used by Network framework
         self.reuseAddress = reuseAddress
         self.tlsOptions = tlsOptions
 
-        self.threadPoolSize = threadPoolSize
         self.noHTTPServer = noHTTPServer
 
-        if let logLevel = logLevel {
+        if let logLevel {
             self.logLevel = logLevel
         } else if let logLevel = env.get("LOG_LEVEL") {
             self.logLevel = Logger.Level(rawValue: logLevel) ?? .info
@@ -162,20 +138,16 @@ public struct HBApplicationConfiguration: Sendable {
         address: HBBindAddress? = nil,
         serverName: String? = nil,
         maxUploadSize: Int? = nil,
-        maxStreamingBufferSize: Int? = nil,
         backlog: Int? = nil,
         reuseAddress: Bool? = nil,
-        threadPoolSize: Int? = nil,
         logLevel: Logger.Level? = nil
     ) -> Self {
         return .init(
             address: address ?? self.address,
             serverName: serverName ?? self.serverName,
             maxUploadSize: maxUploadSize ?? self.maxUploadSize,
-            maxStreamingBufferSize: maxStreamingBufferSize ?? self.maxStreamingBufferSize,
             backlog: backlog ?? self.backlog,
             reuseAddress: reuseAddress ?? self.reuseAddress,
-            threadPoolSize: threadPoolSize ?? self.threadPoolSize,
             logLevel: logLevel ?? self.logLevel
         )
     }
@@ -186,7 +158,6 @@ public struct HBApplicationConfiguration: Sendable {
         return .init(
             address: self.address,
             serverName: self.serverName,
-            maxStreamingBufferSize: self.maxStreamingBufferSize,
             backlog: self.backlog,
             reuseAddress: self.reuseAddress,
             tlsOptions: self.tlsOptions
@@ -197,7 +168,6 @@ public struct HBApplicationConfiguration: Sendable {
         return .init(
             address: self.address,
             serverName: self.serverName,
-            maxStreamingBufferSize: self.maxStreamingBufferSize,
             backlog: self.backlog,
             reuseAddress: self.reuseAddress
         )
--- Sources/HummingbirdCore/Server/ServerConfiguration.swift
+++ Sources/HummingbirdCore/Server/ServerConfiguration.swift
@@ -20,8 +20,6 @@ public struct HBServerConfiguration: Sendable {
     public let address: HBBindAddress
     /// Server name to return in "server" header
     public let serverName: String?
-    /// Maximum size of data in flight while streaming request payloads before back pressure is applied.
-    public let maxStreamingBufferSize: Int
     /// Defines the maximum length for the queue of pending connections
     public let backlog: Int
     /// Allows socket to be bound to an address that is already in use.
@@ -35,19 +33,17 @@ public struct HBServerConfiguration: Sendable {
     /// - Parameters:
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
-    ///   - maxStreamingBufferSize: Maximum size of data in flight while streaming request payloads before back pressure is applied.
+    ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
+    ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
     public init(
         address: HBBindAddress = .hostname(),
         serverName: String? = nil,
-        maxStreamingBufferSize: Int = 1 * 1024 * 1024,
         backlog: Int = 256,
-        reuseAddress: Bool = true,
-        withPipeliningAssistance: Bool = true
+        reuseAddress: Bool = true
     ) {
         self.address = address
         self.serverName = serverName
-        self.maxStreamingBufferSize = maxStreamingBufferSize
         self.backlog = backlog
         self.reuseAddress = reuseAddress
         #if canImport(Network)
@@ -59,21 +55,20 @@ public struct HBServerConfiguration: Sendable {
     /// - Parameters:
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
-    ///   - maxStreamingBufferSize: Maximum size of data in flight while streaming request payloads before back pressure is applied.
+    ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
+    ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
     ///   - tlsOptions: TLS options for when you are using NIOTransportServices
     #if canImport(Network)
     public init(
         address: HBBindAddress = .hostname(),
         serverName: String? = nil,
-        maxStreamingBufferSize: Int = 1 * 1024 * 1024,
         backlog: Int = 256,
         reuseAddress: Bool = true,
         tlsOptions: TSTLSOptions
     ) {
         self.address = address
         self.serverName = serverName
-        self.maxStreamingBufferSize = maxStreamingBufferSize
         self.backlog = backlog
         self.reuseAddress = reuseAddress
         self.tlsOptions = tlsOptions
