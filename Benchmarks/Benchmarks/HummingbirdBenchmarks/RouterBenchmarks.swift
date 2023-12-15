0e4fe0cff464fde3e14901a1fc8b96da55e40fa5
Adam Fowler <adamfowler71@gmail.com>
2023-12-15 08:35:24 +0000

    Benchmarks 2.x (#309)
    * Benchmarks for HB 2.x
    
    * Add Benchmark CI for 2.x.x
    
    * Compare against branch 2.x.x
    
    * swift format
    
    * Add Middleware Chain

---
+++ Benchmarks/Benchmarks/HummingbirdBenchmarks/RouterBenchmarks.swift
@@ -0,0 +1,172 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2021 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Benchmark
+import HTTPTypes
+import Hummingbird
+import NIOHTTPTypes
+@_spi(HBXCT) import HummingbirdCore
+import Logging
+import NIOCore
+import NIOPosix
+
+/// Implementation of a basic request context that supports everything the Hummingbird library needs
+protocol BenchmarkContext: HBBaseRequestContext {
+    ///  Initialize an `HBRequestContext`
+    /// - Parameters:
+    ///   - applicationContext: Context from Application that instigated the request
+    ///   - source: Source of request context
+    ///   - logger: Logger
+    init(
+        applicationContext: HBApplicationContext,
+        eventLoop: EventLoop,
+        allocator: ByteBufferAllocator,
+        logger: Logger
+    )
+}
+
+struct BasicBenchmarkContext: BenchmarkContext {
+    var coreContext: HBCoreRequestContext
+
+    init(
+        applicationContext: HBApplicationContext,
+        eventLoop: EventLoop,
+        allocator: ByteBufferAllocator,
+        logger: Logger
+    ) {
+        self.coreContext = .init(applicationContext: applicationContext, eventLoop: eventLoop, allocator: allocator, logger: logger)
+    }
+}
+
+/// Writes ByteBuffers to AsyncChannel outbound writer
+struct BenchmarkBodyWriter: Sendable, HBResponseBodyWriter {
+    func write(_: ByteBuffer) async throws {}
+}
+
+extension Benchmark {
+    @discardableResult
+    convenience init?<Context: BenchmarkContext>(
+        name: String,
+        context: Context.Type = BasicBenchmarkContext.self,
+        configuration: Benchmark.Configuration = Benchmark.defaultConfiguration,
+        request: HTTPRequest,
+        writeBody: @escaping @Sendable (HBStreamedRequestBody) async throws -> Void = { _ in },
+        setupRouter: @escaping @Sendable (HBRouter<Context>) async throws -> Void
+    ) {
+        let router = HBRouter(context: Context.self)
+        self.init(name, configuration: configuration) { benchmark in
+            let responder = router.buildResponder()
+            let applicationContext = HBApplicationContext(configuration: .init())
+            benchmark.startMeasurement()
+
+            try await withThrowingTaskGroup(of: Void.self) { group in
+                for _ in benchmark.scaledIterations {
+                    let context = Context(applicationContext: applicationContext, eventLoop: MultiThreadedEventLoopGroup.singleton.any(), allocator: ByteBufferAllocator(), logger: Logger(label: "Benchmark"))
+                    let requestBodyStream = HBStreamedRequestBody()
+                    let requestBody = HBRequestBody.stream(requestBodyStream)
+                    let hbRequest = HBRequest(head: request, body: requestBody)
+                    group.addTask {
+                        let response = try await responder.respond(to: hbRequest, context: context)
+                        try await response.body.write(BenchmarkBodyWriter())
+                    }
+                    try await writeBody(requestBodyStream)
+                    requestBodyStream.finish()
+                }
+            }
+        } setup: {
+            try await setupRouter(router)
+            Self.blackHole(MultiThreadedEventLoopGroup.singleton.any())
+        }
+    }
+}
+
+extension HTTPField.Name {
+    static let test = Self("Test")!
+}
+
+func routerBenchmarks() {
+    Benchmark(
+        name: "GET NoResponse",
+        request: .init(method: .get, scheme: "http", authority: "localhost", path: "/")
+    ) { router in
+        router.get { _, _ in
+            HTTPResponse.Status.ok
+        }
+    }
+
+    Benchmark(
+        name: "Middleware Chain",
+        request: .init(method: .get, scheme: "http", authority: "localhost", path: "/")
+    ) { router in
+        struct EmptyMiddleware<Context>: HBMiddlewareProtocol {
+            func handle(_ request: HBRequest, context: Context, next: (HBRequest, Context) async throws -> HBResponse) async throws -> HBResponse {
+                return try await next(request, context)
+            }
+        }
+        router.middlewares.add(EmptyMiddleware())
+        router.middlewares.add(EmptyMiddleware())
+        router.middlewares.add(EmptyMiddleware())
+        router.middlewares.add(EmptyMiddleware())
+        router.get { _, _ in
+            HTTPResponse.Status.ok
+        }
+    }
+
+    Benchmark(
+        name: "Get Response",
+        request: .init(method: .get, scheme: "http", authority: "localhost", path: "/")
+    ) { router in
+        router.get { _, _ in
+            HBResponse(status: .ok, headers: [:], body: .init { writer in
+                try await writer.write(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+                try await writer.write(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+                try await writer.write(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+                try await writer.write(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+            })
+        }
+    }
+
+    Benchmark(
+        name: "PUT",
+        request: .init(method: .put, scheme: "http", authority: "localhost", path: "/")
+    ) { bodyStream in
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+    } setupRouter: { router in
+        router.put { request, _ in
+            let body = try await request.body.collect(upTo: .max)
+            return body.readableBytes.description
+        }
+    }
+
+    Benchmark(
+        name: "Echo",
+        request: .init(method: .post, scheme: "http", authority: "localhost", path: "/")
+    ) { bodyStream in
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+        await bodyStream.send(ByteBufferAllocator().buffer(repeating: 0, count: 16000))
+    } setupRouter: { router in
+        router.post { request, _ in
+            HBResponse(status: .ok, headers: [:], body: .init { writer in
+                for try await buffer in request.body {
+                    try await writer.write(buffer)
+                }
+            })
+        }
+    }
+}
--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,33 +1,27 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Benchmarks",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     dependencies: [
-        .package(url: "https://github.com/ordo-one/package-benchmark.git", .upToNextMajor(from: "0.9.0")),
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", .upToNextMajor(from: "1.0.0")),
         .package(path: "../../hummingbird"),
     ],
     targets: [
-        // Support target having fundamentally verbatim copies of NIOPerformanceTester sources
-        .target(
-            name: "HBPerformance",
-            dependencies: [
-                .product(name: "BenchmarkSupport", package: "package-benchmark"),
-                .product(name: "Hummingbird", package: "hummingbird"),
-            ]
-        ),
-
         // Benchmark targets
         .executableTarget(
             name: "HummingbirdBenchmarks",
             dependencies: [
-                "HBPerformance",
-                .product(name: "BenchmarkSupport", package: "package-benchmark"),
+                .product(name: "Benchmark", package: "package-benchmark"),
+                .product(name: "Hummingbird", package: "hummingbird"),
             ],
-            path: "Benchmarks/HummingbirdBenchmarks"
+            path: "Benchmarks/HummingbirdBenchmarks",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark"),
+            ]
         ),
     ]
 )
