6389db7ee8d2f68e3000dd0c3eb4fb589f9db046
Adam Fowler <adamfowler71@gmail.com>
2023-12-16 09:11:08 +0000

    Add swift-atomics dependency

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0"..<"3.0.0"),
@@ -35,6 +36,7 @@ let package = Package(
             .byName(name: "HummingbirdCore"),
             .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
             .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            .product(name: "Atomics", package: "swift-atomics"),
             .product(name: "HTTPTypes", package: "swift-http-types"),
             .product(name: "Logging", package: "swift-log"),
             .product(name: "Metrics", package: "swift-metrics"),
