02ab65198bbf1b0c2c1087008f2c0f308d804dd5
Adam Fowler <adamfowler71@gmail.com>
2023-12-31 18:18:18 +0100

    Add RequestID to generate unique Identifier for each request (#326)
    * Add RequestID to generate unique Identifier for each request
    
    This will also generate unique identifiers across multiple instances of a Humminbird application
    
    * Don't bother with leading zeros for high value in request id
    
    * Don't need to store high in RequestID as it is the same for all ids
    
    * Don't keep rebuilding high string

---
+++ Sources/Hummingbird/Server/RequestID.swift
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2023 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Atomics
+
+/// Generate Unique ID for each request
+struct RequestID: CustomStringConvertible {
+    let low: UInt64
+
+    init() {
+        self.low = Self.globalRequestID.loadThenWrappingIncrement(by: 1, ordering: .relaxed)
+    }
+
+    var description: String {
+        Self.high + self.formatAsHexWithLeadingZeros(self.low)
+    }
+
+    func formatAsHexWithLeadingZeros(_ value: UInt64) -> String {
+        let string = String(value, radix: 16)
+        if string.count < 16 {
+            return String(repeating: "0", count: 16 - string.count) + string
+        } else {
+            return string
+        }
+    }
+
+    private static let high = String(UInt64.random(in: .min ... .max), radix: 16)
+    private static let globalRequestID = ManagedAtomic<UInt64>(UInt64.random(in: .min ... .max))
+}
