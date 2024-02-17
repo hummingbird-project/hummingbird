d052e37f2ab4cbaa99825c90a741e8ce4b0aa9e7
Adam Fowler <adamfowler71@gmail.com>
2024-02-17 08:38:47 +0000

    Update swiftformat version in validate.sh

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -13,7 +13,7 @@
 ##
 ##===----------------------------------------------------------------------===##
 
-SWIFT_FORMAT_VERSION=0.48.17
+SWIFT_FORMAT_VERSION=0.51.15
 
 set -eu
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
