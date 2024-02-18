5a578e6e6514b0e0bdbbdcd9eea9b44e2edcf81d
Adam Fowler <adamfowler71@gmail.com>
2024-02-18 07:29:59 +0000

    Disable typeSugar swiftformat rule

--- Sources/Hummingbird/Server/Request.swift
+++ Sources/Hummingbird/Server/Request.swift
@@ -66,7 +66,7 @@ private extension CodingKey {
     }
 }
 
-private extension [CodingKey] {
+private extension Array<CodingKey> {
     /// returns a path key using a dot character as a separator
     var pathKeyValue: String {
         map(\.pathKeyValue).joined(separator: ".")
