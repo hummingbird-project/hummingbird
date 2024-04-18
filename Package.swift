c33220bf0d229eb127cb4a998c99c0d7554fd641
Adam Fowler <adamfowler71@gmail.com>
2024-04-18 16:29:04 +0100

    Use hummingbird-core v1.6.1

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-distributed-tracing.git", from: "1.0.1"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.58.0"),
         .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha.9"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", from: "1.2.1"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-core.git", from: "1.6.1"),
     ],
     targets: [
         .target(name: "Hummingbird", dependencies: [
