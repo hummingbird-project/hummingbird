097a686d283e8e7471235e6956de87c63edd43f9
Adam Fowler <adamfowler71@gmail.com>
2024-05-06 14:51:32 +0100

    swift format

--- Sources/HummingbirdTesting/TestApplication.swift
+++ Sources/HummingbirdTesting/TestApplication.swift
@@ -37,11 +37,12 @@ internal struct TestApplication<BaseApp: ApplicationProtocol>: ApplicationProtoc
     /// Event loop group used by application
     var eventLoopGroup: EventLoopGroup { self.base.eventLoopGroup }
     /// Configuration
-    var configuration: ApplicationConfiguration { 
-        var configuration = base.configuration
+    var configuration: ApplicationConfiguration {
+        var configuration = self.base.configuration
         configuration.address = .hostname("localhost", port: 0)
         return configuration
     }
+
     /// Logger
     var logger: Logger { self.base.logger }
     /// On server running
