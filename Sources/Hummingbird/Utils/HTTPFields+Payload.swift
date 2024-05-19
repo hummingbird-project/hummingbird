90751b11521322dcdb1b0052ad4002780464065d
Joannis Orlandos <joannis@orlandos.nl>
2024-05-19 23:39:23 +0200

    Remove separate init() + reserveCapacity on HTTPFields, as it's much slower than expected

--- Sources/Hummingbird/Utils/HTTPFields+Payload.swift
+++ Sources/Hummingbird/Utils/HTTPFields+Payload.swift
@@ -2,13 +2,9 @@ import HTTPTypes
 
 extension HTTPFields {
     init(contentType: String, contentLength: Int) {
-        self.init()
-
-        // Content-Type, Content-Length, Server, Date + 2 extra headers
-        // This should cover our expected amount of headers
-        self.reserveCapacity(6)
-
-        self[.contentType] = contentType
-        self[.contentLength] = String(describing: contentLength)
+        self = [
+            .contentType: contentType,
+            .contentLength: String(describing: contentLength)
+        ]
     }
 }
