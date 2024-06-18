f92ec259a4dc28751b19460b1739f31d446124dd
Joannis Orlandos <joannis@orlandos.nl>
2024-06-18 11:04:26 +0200

    Fix the catch-all router bug, and the Tibor's bug where we need to find different paths
    Move all logic into a ResolveContext. When encountering a dead end, the router goes back up the chain and descends another child. When a catch-all finds a new route to descend, store the nextNodeIndex so that the path can be descended further

--- Sources/Hummingbird/Router/Trie/Trie+resolve.swift
+++ Sources/Hummingbird/Router/Trie/Trie+resolve.swift
@@ -18,24 +18,81 @@ extension RouterTrie {
     /// Resolve a path to a `Value` if available
     @inlinable
     public func resolve(_ path: String) -> (value: Value, parameters: Parameters)? {
-        let pathComponents = path.split(separator: "/", omittingEmptySubsequences: true)
-        var pathComponentsIterator = pathComponents.makeIterator()
-        var parameters = Parameters()
+        var context = ResolveContext(path: path, trie: trie, values: values)
+        return context.resolve()
+    }
 
-        var node = trie.nodes[0]
-        var nodeIndex = 1
+    @usableFromInline
+    struct ResolveContext {
+        @usableFromInline let path: String
+        @usableFromInline let pathComponents: [Substring]
+        @usableFromInline let trie: Trie
+        @usableFromInline let values: [Value?]
+        @usableFromInline var parameters = Parameters()
 
-        while let component = pathComponentsIterator.next() {
-            node = self.matchComponent(component, atNodeIndex: &nodeIndex, parameters: &parameters)
+        @usableFromInline init(path: String, trie: Trie, values: [Value?]) {
+            self.path = path
+            self.trie = trie
+            self.pathComponents = path.split(separator: "/", omittingEmptySubsequences: true)
+            self.values = values
+        }
+
+        @usableFromInline func nextPathComponent(advancingIndex index: inout Int) -> Substring? {
+            if index >= pathComponents.count {
+                return nil
+            }
+
+            let component = pathComponents[index]
+            index += 1
+            return component
+        }
+
+        @inlinable
+        mutating func resolve() -> (value: Value, parameters: Parameters)? {
+            guard let component = pathComponents.first else {
+                guard let value = values[trie.nodes[0].valueIndex] else {
+                    return nil
+                }
+
+                return (value: value, parameters: parameters)
+            }
+
+            var nextNodeIndex = 1
+            guard let node = descend(
+                component: component,
+                nextPathComponentIndex: 1,
+                nextNodeIndex: &nextNodeIndex
+            ) else {
+                return nil
+            }
+
+            if let value = values[node.valueIndex] {
+                return (value: value, parameters: parameters)
+            } else {
+                return nil
+            }
+        }
+
+        @inlinable
+        mutating func descend(
+            component: Substring,
+            nextPathComponentIndex: Int,
+            nextNodeIndex: inout Int
+        ) -> TrieNode? {
+            var node = self.matchComponent(component, atNodeIndex: &nextNodeIndex)
+            var nextPathComponentIndex = nextPathComponentIndex
+
             if node.token == .recursiveWildcard {
                 // we have found a recursive wildcard. Go through all the path components until we match one of them
                 // or reach the end of the path component array
                 var range = component.startIndex..<component.endIndex
-                while let component = pathComponentsIterator.next() {
-                    var nodeIndex = nodeIndex
-                    let recursiveNode = self.matchComponent(component, atNodeIndex: &nodeIndex, parameters: &parameters)
+
+                while let component = nextPathComponent(advancingIndex: &nextPathComponentIndex) {
+                    var _nextNodeIndex = nextNodeIndex
+                    let recursiveNode = self.matchComponent(component, atNodeIndex: &_nextNodeIndex)
                     if recursiveNode.token != .deadEnd {
                         node = recursiveNode
+                        nextNodeIndex = _nextNodeIndex
                         break
                     }
                     // extend range of catch all text
@@ -43,105 +100,115 @@ extension RouterTrie {
                 }
                 parameters.setCatchAll(path[range])
             }
+
             if node.token == .deadEnd {
                 return nil
             }
-        }
 
-        if let value = self.values[node.valueIndex] {
-            return (value: value, parameters: parameters)
-        } else {
-            return nil
-        }
-    }
+            if let nextComponent = nextPathComponent(advancingIndex: &nextPathComponentIndex) {
+                // There's another component to the route
 
-    /// Match sibling node for path component
-    @inlinable
-    internal func matchComponent(
-        _ component: Substring,
-        atNodeIndex nodeIndex: inout Int,
-        parameters: inout Parameters
-    ) -> TrieNode {
-        while nodeIndex < trie.nodes.count {
-            let node = trie.nodes[nodeIndex]
-            let result = self.matchComponent(
-                component,
-                node: node,
-                parameters: &parameters
-            )
-            switch result {
-            case .match, .deadEnd:
-                nodeIndex += 1
+                // Keep track of the nextNodeIndex known locally
+                // As we descend, this value changes. However, if descending doesn't yield a result
+                // We'll want to continue on the next path
+                let lastKnownNextNodeIndex = nextNodeIndex
+                var nextIndex = nextNodeIndex
+
+                // If a dead end is found, we're done
+                while trie.nodes[nextIndex].token != .deadEnd {
+                    if let node = descend(
+                        component: nextComponent,
+                        nextPathComponentIndex: nextPathComponentIndex,
+                        nextNodeIndex: &nextNodeIndex
+                    ) {
+                        return node
+                    }
+                    nextIndex = trie.nodes[nextIndex].nextSiblingNodeIndex
+                    nextNodeIndex = nextIndex
+                }
+
+                return nil
+            } else {
                 return node
-            default:
-                nodeIndex = Int(node.nextSiblingNodeIndex)
             }
         }
 
-        // should never get here
-        return TrieNode(valueIndex: 0, token: .deadEnd, nextSiblingNodeIndex: .max)
-    }
-
-    @usableFromInline
-    enum MatchResult {
-        case match, mismatch, ignore, deadEnd
-    }
-
-    @inlinable
-    func matchComponent(
-        _ component: Substring,
-        node: TrieNode,
-        parameters: inout Parameters
-    ) -> MatchResult {
-        switch node.token {
-        case .path(let constant):
-            // The current node is a constant
-            if trie.stringValues[Int(constant)] == component {
-                return .match
+        /// Match sibling node for path component
+        @inlinable
+        mutating func matchComponent(_ component: Substring, atNodeIndex nextNodeIndex: inout Int) -> TrieNode {
+            while nextNodeIndex < trie.nodes.count {
+                let node = trie.nodes[nextNodeIndex]
+                let result = self.matchComponent(component, node: node)
+                switch result {
+                case .match, .deadEnd:
+                    nextNodeIndex += 1
+                    return node
+                default:
+                    nextNodeIndex = Int(node.nextSiblingNodeIndex)
+                }
             }
 
-            return .mismatch
-        case .capture(let parameter):
-            parameters[trie.stringValues[Int(parameter)]] = component
-            return .match
-        case .prefixCapture(let parameter, let suffix):
-            let suffix = trie.stringValues[Int(suffix)]
+            // should never get here
+            return TrieNode(valueIndex: 0, token: .deadEnd, nextSiblingNodeIndex: .max)
+        }
 
-            if component.hasSuffix(suffix) {
-                parameters[trie.stringValues[Int(parameter)]] = component.dropLast(suffix.count)
+        @usableFromInline
+        enum MatchResult {
+            case match, mismatch, ignore, deadEnd
+        }
+
+        @inlinable
+        mutating func matchComponent(_ component: Substring, node: TrieNode) -> MatchResult {
+            switch node.token {
+            case .path(let constant):
+                // The current node is a constant
+                if trie.stringValues[Int(constant)] == component {
+                    return .match
+                }
+
+                return .mismatch
+            case .capture(let parameter):
+                parameters[trie.stringValues[Int(parameter)]] = component
                 return .match
-            }
+            case .prefixCapture(let parameter, let suffix):
+                let suffix = trie.stringValues[Int(suffix)]
 
-            return .mismatch
-        case .suffixCapture(let prefix, let parameter):
-            let prefix = trie.stringValues[Int(prefix)]
-            if component.hasPrefix(prefix) {
-                parameters[trie.stringValues[Int(parameter)]] = component.dropFirst(prefix.count)
+                if component.hasSuffix(suffix) {
+                    parameters[trie.stringValues[Int(parameter)]] = component.dropLast(suffix.count)
+                    return .match
+                }
+
+                return .mismatch
+            case .suffixCapture(let prefix, let parameter):
+                let prefix = trie.stringValues[Int(prefix)]
+                if component.hasPrefix(prefix) {
+                    parameters[trie.stringValues[Int(parameter)]] = component.dropFirst(prefix.count)
+                    return .match
+                }
+
+                return .mismatch
+            case .wildcard:
+                // Always matches, descend
                 return .match
-            }
+            case .prefixWildcard(let suffix):
+                if component.hasSuffix(trie.stringValues[Int(suffix)]) {
+                    return .match
+                }
 
-            return .mismatch
-        case .wildcard:
-            // Always matches, descend
-            return .match
-        case .prefixWildcard(let suffix):
-            if component.hasSuffix(trie.stringValues[Int(suffix)]) {
+                return .mismatch
+            case .suffixWildcard(let prefix):
+                if component.hasPrefix(trie.stringValues[Int(prefix)]) {
+                    return .match
+                }
+
+                return .mismatch
+            case .recursiveWildcard:
                 return .match
+            case .null:
+                return .ignore
+            case .deadEnd:
+                return .deadEnd
             }
-
-            return .mismatch
-        case .suffixWildcard(let prefix):
-            if component.hasPrefix(trie.stringValues[Int(prefix)]) {
-                return .match
-            }
-
-            return .mismatch
-        case .recursiveWildcard:
-            return .match
-        case .null:
-            return .ignore
-        case .deadEnd:
-            return .deadEnd
         }
     }
 }
