de83f4901eeb011d65f85bb440d136dad53ef5ee
Adam Fowler <adamfowler71@gmail.com>
2024-08-09 11:52:25 +0100

    Comment updates

--- Sources/Hummingbird/Configuration.swift
+++ Sources/Hummingbird/Configuration.swift
@@ -33,7 +33,8 @@ public struct ApplicationConfiguration: Sendable {
     public var backlog: Int
     /// Allows socket to be bound to an address that is already in use.
     public var reuseAddress: Bool
-    /// object deciding on when we should accept new connection
+    /// Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    /// to set the maximum allowed connections.
     public var availableConnectionsDelegate: AvailableConnectionsDelegate?
     #if canImport(Network)
     /// TLS options for NIO Transport services
@@ -50,7 +51,8 @@ public struct ApplicationConfiguration: Sendable {
     ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
     ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: object deciding on when we should accept new connection
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    ///         to set the maximum allowed connections.
     public init(
         address: BindAddress = .hostname(),
         serverName: String? = nil,
@@ -75,7 +77,8 @@ public struct ApplicationConfiguration: Sendable {
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: object deciding on when we should accept new connection
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    ///         to set the maximum allowed connections.
     ///   - tlsOptions: TLS options for when you are using NIOTransportServices
     public init(
         address: BindAddress = .hostname(),
--- Sources/HummingbirdCore/Server/ServerConfiguration.swift
+++ Sources/HummingbirdCore/Server/ServerConfiguration.swift
@@ -24,7 +24,12 @@ public struct ServerConfiguration: Sendable {
     public let backlog: Int
     /// Allows socket to be bound to an address that is already in use.
     public let reuseAddress: Bool
-    /// object deciding on when we should accept new connection
+    /// Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    /// to set the maximum allowed connections.
+    /// 
+    /// If we are not accepting connections the requests will be added to the backlog until the 
+    /// server is accepting connections again. If the backlog fills up then the server will start
+    /// refusing requests.
     public let availableConnectionsDelegate: AvailableConnectionsDelegate?
     #if canImport(Network)
     /// TLS options for NIO Transport services
@@ -38,7 +43,8 @@ public struct ServerConfiguration: Sendable {
     ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
     ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: object deciding on when we should accept new connection
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    ///         to set the maximum allowed connections.
     public init(
         address: BindAddress = .hostname(),
         serverName: String? = nil,
@@ -63,7 +69,8 @@ public struct ServerConfiguration: Sendable {
     ///   - backlog: the maximum length for the queue of pending connections.  If a connection request arrives with the queue full,
     ///         the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: object deciding on when we should accept new connection
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use ``MaximumAvailableConnections``
+    ///         to set the maximum allowed connections.
     ///   - tlsOptions: TLS options for when you are using NIOTransportServices
     #if canImport(Network)
     public init(
