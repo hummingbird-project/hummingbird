d0096e81e678dccebeebe1d3e3422535d791d6e7
Adam Fowler <adamfowler71@gmail.com>
2024-09-05 12:45:26 +0100

    swift format

--- Sources/Hummingbird/Configuration.swift
+++ Sources/Hummingbird/Configuration.swift
@@ -48,10 +48,10 @@ public struct ApplicationConfiguration: Sendable {
     /// - Parameters:
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
-    ///   - backlog: the maximum length for the queue of pending connections.  If a connection request 
+    ///   - backlog: the maximum length for the queue of pending connections.  If a connection request
     ///         arrives with the queue full, the client may receive an error with an indication of ECONNREFUSE
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use 
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use
     ///         ``HummingbirdCore/MaximumAvailableConnections`` to set the maximum allowed connections.
     public init(
         address: BindAddress = .hostname(),
@@ -77,7 +77,7 @@ public struct ApplicationConfiguration: Sendable {
     ///   - address: Bind address for server
     ///   - serverName: Server name to return in "server" header
     ///   - reuseAddress: Allows socket to be bound to an address that is already in use.
-    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use 
+    ///   - availableConnectionsDelegate: Object deciding on when we should accept new connection. Use
     ///         ``HummingbirdCore/MaximumAvailableConnections`` to set the maximum allowed connections.
     ///   - tlsOptions: TLS options for when you are using NIOTransportServices
     public init(
