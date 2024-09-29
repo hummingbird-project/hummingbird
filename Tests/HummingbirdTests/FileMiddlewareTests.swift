0e84f21e2c93acdc69d34493415cf6c961211d92
Joannis Orlandos <joannis@orlandos.nl>
2024-09-29 12:33:03 +0200

    Add tests

--- Tests/HummingbirdTests/FileMiddlewareTests.swift
+++ Tests/HummingbirdTests/FileMiddlewareTests.swift
@@ -316,6 +316,55 @@ final class FileMiddlewareTests: XCTestCase {
         }
     }
 
+    func testOnThrow404() async throws {
+        let router = Router()
+        router.middlewares.add(FileMiddleware(".", searchForIndexHtml: true))
+        router.get { _, _ -> String in
+            throw HTTPError(.notFound)
+        }
+        let app = Application(responder: router.buildResponder())
+
+        let text = "Test file contents"
+        let data = Data(text.utf8)
+        let fileURL = URL(fileURLWithPath: "index.html")
+        XCTAssertNoThrow(try data.write(to: fileURL))
+        defer { XCTAssertNoThrow(try FileManager.default.removeItem(at: fileURL)) }
+
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/", method: .get) { response in
+                XCTAssertEqual(String(buffer: response.body), text)
+            }
+        }
+    }
+
+    func testOnThrowCustom404() async throws {
+        let router = Router()
+        router.middlewares.add(FileMiddleware(".", searchForIndexHtml: true))
+        struct Custom404Error: HTTPResponseError {
+            var status: HTTPResponse.Status { .notFound }
+
+            func response(from request: Request, context: some RequestContext) throws -> Response {
+                Response(status: status)
+            }
+        }
+        router.get { _, _ -> String in
+            throw Custom404Error()
+        }
+        let app = Application(responder: router.buildResponder())
+
+        let text = "Test file contents"
+        let data = Data(text.utf8)
+        let fileURL = URL(fileURLWithPath: "index.html")
+        XCTAssertNoThrow(try data.write(to: fileURL))
+        defer { XCTAssertNoThrow(try FileManager.default.removeItem(at: fileURL)) }
+
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/", method: .get) { response in
+                XCTAssertEqual(String(buffer: response.body), text)
+            }
+        }
+    }
+
     func testFolder() async throws {
         let router = Router()
         router.middlewares.add(FileMiddleware(".", searchForIndexHtml: false))
