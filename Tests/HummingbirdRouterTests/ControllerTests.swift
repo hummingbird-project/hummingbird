b46b2d74c5f669758f53b145c97694f10d54e476
Connor Ricks <connor.ricks@gmail.com>
2024-10-01 00:30:19 -0400

    Add route controller for better composability. (#565)

---
+++ Tests/HummingbirdRouterTests/ControllerTests.swift
@@ -0,0 +1,103 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2023 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Hummingbird
+import HummingbirdRouter
+import HummingbirdTesting
+import XCTest
+
+final class ControllerTests: XCTestCase {
+    func testRouterControllerWithSingleRoute() async throws {
+        struct TestController: RouterController {
+            typealias Context = BasicRouterRequestContext
+            var body: some RouterMiddleware<Context> {
+                Get("foo") { _,_ in "foo" }
+            }
+        }
+
+        let router = RouterBuilder(context: BasicRouterRequestContext.self) {
+            TestController()
+        }
+
+        let app = Application(responder: router)
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/foo", method: .get) {
+                XCTAssertEqual(String(buffer: $0.body), "foo")
+            }
+        }
+    }
+
+    func testRouterControllerWithMultipleRoutes() async throws {
+        struct TestController: RouterController {
+            typealias Context = BasicRouterRequestContext
+            var body: some RouterMiddleware<Context> {
+                Get("foo") { _,_ in "foo" }
+                Get("bar") { _,_ in "bar" }
+            }
+        }
+
+        let router = RouterBuilder(context: BasicRouterRequestContext.self) {
+            TestController()
+        }
+
+        let app = Application(responder: router)
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/foo", method: .get) {
+                XCTAssertEqual(String(buffer: $0.body), "foo")
+            }
+
+            try await client.execute(uri: "/bar", method: .get) {
+                XCTAssertEqual(String(buffer: $0.body), "bar")
+            }
+        }
+    }
+
+    func testRouterControllerWithGenericChildren() async throws {
+        struct ChildController: RouterController {
+            typealias Context = BasicRouterRequestContext
+            let name: String
+            var body: some RouterMiddleware<Context> {
+                Get("child_\(name)") { _,_ in "child_\(name)" }
+            }
+        }
+
+        struct ParentController<Context: RouterRequestContext, Child: RouterMiddleware>: RouterController where Child.Context == Context {
+            @MiddlewareFixedTypeBuilder<Request, Response, Context> var child: () -> Child
+
+            var body: some RouterMiddleware<Context> {
+                RouteGroup("parent") {
+                    child()
+                }
+            }
+        }
+
+        let router = RouterBuilder(context: BasicRouterRequestContext.self) {
+            ParentController {
+                Get("child_a") { _,_ in "child_a"}
+                ChildController(name: "b")
+                ChildController(name: "c")
+                Get("child_d") { _,_ in "child_d"}
+            }
+        }
+
+        let app = Application(responder: router)
+        try await app.test(.router) { client in
+            for letter in "abcd" {
+                try await client.execute(uri: "/parent/child_\(letter)", method: .get) {
+                    XCTAssertEqual(String(buffer: $0.body), "child_\(letter)")
+                }
+            }
+        }
+    }
+}
