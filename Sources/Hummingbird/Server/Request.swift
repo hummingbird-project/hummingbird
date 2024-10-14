ed18ca8af0372a9fcc96a1261a6ee3df605d0de4
Adam Fowler <adamfowler71@gmail.com>
2024-10-14 12:03:38 +0100

    Add Request.decodeQuery

--- Sources/Hummingbird/Server/Request.swift
+++ Sources/Hummingbird/Server/Request.swift
@@ -55,6 +55,34 @@ extension Request {
             throw error
         }
     }
+
+    /// Decode request query using ``URLEncodedFormDecoder``.
+    /// - Parameters
+    ///   - type: Type you want to decode to
+    ///   - context: Request context
+    public func decodeQuery<Type: Decodable>(as type: Type.Type, context: some RequestContext) throws -> Type {
+        do {
+            return try URLEncodedFormDecoder().decode(Type.self, from: self.uri.query ?? "")
+        } catch DecodingError.dataCorrupted(_) {
+            let message = "The given data was not valid input."
+            throw HTTPError(.badRequest, message: message)
+        } catch DecodingError.keyNotFound(let key, _) {
+            let path = key.pathKeyValue
+            let message = "Coding key `\(path)` not found."
+            throw HTTPError(.badRequest, message: message)
+        } catch DecodingError.valueNotFound(_, let context) {
+            let path = context.codingPath.pathKeyValue
+            let message = "Value not found for `\(path)` key."
+            throw HTTPError(.badRequest, message: message)
+        } catch DecodingError.typeMismatch(let type, let context) {
+            let path = context.codingPath.pathKeyValue
+            let message = "Type mismatch for `\(path)` key, expected `\(type)` type."
+            throw HTTPError(.badRequest, message: message)
+        } catch let error as HTTPResponseError {
+            context.logger.debug("Decode Error: \(error)")
+            throw error
+        }
+    }
 }
 
 private extension CodingKey {
