ac00a22e6f4b57f62b2ba30acd46550b159749dd
Connor Ricks <13373737+connor-ricks@users.noreply.github.com>
2024-10-15 03:47:59 -0400

    Add additional result builder capabilities. (#584)
    * Add additional result builder capabilities.
    
    - Adds support for conditional if-else statement syntax in result builders
    - Adds support for conditional if statement syntax in result builders
    - Adds support for for loop statement syntax in result builders
    - Adds support for optional unwrapping statement syntax in result builders.
    - Adds tests for validating the additional supported syntax.
    - Runs script for updating contributors.
    
    * Consolidate Conditional/Empty into Optional Middleware.
    
    * Hide result builder helper types from documentation.
    
    ---------
    
    Co-authored-by: Adam Fowler <adamfowler71@gmail.com>

---
+++ Sources/Hummingbird/Middleware/MiddlewareModule/_OptionalMiddleware.swift
@@ -0,0 +1,48 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2024 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// A middleware that can handle an optional middleware.
+///
+/// This middleware is useful for situations where you want to optionally unwrap a middleware.
+///
+/// You won't typically construct this middleware directly, but instead will use standard `if`-`else`
+/// statements in a parser builder to automatically build conditional middleware:
+///
+/// ```swift
+/// router.addMiddleware {
+///   if let middleware {
+///     middleware
+///   }
+///   ...
+/// }
+/// ```
+@_documentation(visibility: internal)
+public struct _OptionalMiddleware<M0: MiddlewareProtocol>: MiddlewareProtocol {
+    public typealias Input = M0.Input
+    public typealias Output = M0.Output
+    public typealias Context = M0.Context
+
+    public let middleware: M0?
+
+    @inlinable
+    public func handle(_ input: M0.Input, context: M0.Context, next: (M0.Input, M0.Context) async throws -> M0.Output) async throws -> M0.Output {
+        guard let middleware else {
+            return try await next(input, context)
+        }
+
+        return try await middleware.handle(input, context: context, next: next)
+    }
+}
+
+extension _OptionalMiddleware: RouterMiddleware where M0.Input == Request, M0.Output == Response {}
