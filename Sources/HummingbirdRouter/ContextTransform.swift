b701a95a3895509212e9ca8b9afa4c2918bcae93
Adam Fowler <adamfowler71@gmail.com>
2024-10-23 09:44:35 +0100

    Add ThrowingContextTransform to HummingbirdRouter

--- Sources/HummingbirdRouter/ContextTransform.swift
+++ Sources/HummingbirdRouter/ContextTransform.swift
@@ -61,3 +61,51 @@ public struct ContextTransform<Context: RouterRequestContext, HandlerContext: Ro
         }
     }
 }
+
+/// Router middleware that transforms the ``Hummingbird/RequestContext`` and uses it with the contained
+/// Middleware chain
+///
+/// For the transform to work the `Source` of the transformed `RequestContext`` needs to be
+/// the original `RequestContext` eg
+/// ```
+/// struct TransformedRequestContext {
+///     typealias Source = BasicRequestContext
+///     var coreContext: CoreRequestContextStorage
+///     init(source: Source) {
+///         self.coreContext = .init(source: source)
+///     }
+/// }
+/// ```
+public struct ThrowingContextTransform<Context: RouterRequestContext, HandlerContext: RouterRequestContext & ChildRequestContext, Handler: MiddlewareProtocol>: RouterMiddleware where Handler.Input == Request, Handler.Output == Response, Handler.Context == HandlerContext, HandlerContext.ParentContext == Context {
+    public typealias Input = Request
+    public typealias Output = Response
+
+    /// Group handler
+    @usableFromInline
+    let handler: Handler
+
+    /// Create RouteGroup from result builder
+    /// - Parameters:
+    ///   - context: RequestContext to convert to
+    ///   - builder: RouteGroup builder
+    public init(
+        to context: HandlerContext.Type,
+        @MiddlewareFixedTypeBuilder<Request, Response, HandlerContext> builder: () -> Handler
+    ) {
+        self.handler = builder()
+    }
+
+    /// Process HTTP request and return an HTTP response
+    /// - Parameters:
+    ///   - input: Request
+    ///   - context: Request context
+    ///   - next: Next middleware to run, if no route handler is found
+    /// - Returns: Response
+    @inlinable
+    public func handle(_ input: Input, context: Context, next: (Input, Context) async throws -> Output) async throws -> Output {
+        let handlerContext = try Handler.Context(context: context)
+        return try await self.handler.handle(input, context: handlerContext) { input, _ in
+            try await next(input, context)
+        }
+    }
+}
