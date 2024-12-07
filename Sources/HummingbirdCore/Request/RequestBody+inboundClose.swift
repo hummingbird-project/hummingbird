56851782d6e2a120b56b6e8322475da421e099f6
Adam Fowler <adamfowler71@gmail.com>
2024-12-07 22:47:57 +0000

    onInboundClosed is Sendable so don't need to wrap it

--- Sources/HummingbirdCore/Request/RequestBody+inboundClose.swift
+++ Sources/HummingbirdCore/Request/RequestBody+inboundClose.swift
@@ -100,12 +100,11 @@ extension RequestBody {
         onInboundClosed: @Sendable @escaping () -> Void
     ) async throws -> Value {
         let unsafeIterator = UnsafeTransfer(iterator)
-        let unsafeOnInboundClosed = UnsafeTransfer(onInboundClosed)
         let value = try await withThrowingTaskGroup(of: Void.self) { group in
             group.addTask {
                 do {
                     if try await self.iterate(iterator: unsafeIterator.wrappedValue, source: source) == .inboundClosed {
-                        unsafeOnInboundClosed.wrappedValue()
+                        onInboundClosed()
                     }
                 } catch is CancellationError {}
             }
