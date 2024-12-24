09d6e2fd1d25682164d266e04a66aa34a7618271
Adam Fowler <adamfowler71@gmail.com>
2024-12-24 12:50:32 +0000

    Copyright header

--- Sources/Hummingbird/Codable/URLEncodedForm/URLEncodedFormNode.swift
+++ Sources/Hummingbird/Codable/URLEncodedForm/URLEncodedFormNode.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Hummingbird server framework project
+//
+// Copyright (c) 2021-2024 the Hummingbird authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See hummingbird/CONTRIBUTORS.txt for the list of Hummingbird authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 /// Internal representation of URL encoded form data used by both encode and decode
 enum URLEncodedFormNode: CustomStringConvertible, Equatable {
     /// holds a value
