f2e6c346a8425e1062c2a921d98dfa20c6224bc2
Adam Fowler <adamfowler71@gmail.com>
2025-01-07 08:39:34 +0000

    Fix other http header dates (eg FileMiddleware output)

--- Sources/Hummingbird/Utils/DateCache+Date.swift
+++ Sources/Hummingbird/Utils/DateCache+Date.swift
@@ -20,7 +20,7 @@ extension DateCache {
     static var rfc1123Formatter: DateFormatter {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
-        formatter.dateFormat = "EEE, d MMM yyy HH:mm:ss z"
+        formatter.dateFormat = "EEE, dd MMM yyy HH:mm:ss z"
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
     }
