95598edb6dc4c816a1cbbe7b580322af3ef8892a
Benedek Kozma <cyberbeni@gmail.com>
2025-02-27 16:54:25 +0100

    format files

--- Tests/HummingbirdTests/CacheControlTests.swift
+++ Tests/HummingbirdTests/CacheControlTests.swift
@@ -4,23 +4,23 @@ import XCTest
 final class CacheControlTests: XCTestCase {
     func testCssIsText() {
         let cacheControl = CacheControl([
-			(MediaType(type: .text), [.noCache, .public]),
-		])
+            (MediaType(type: .text), [.noCache, .public])
+        ])
         XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-cache, public")
     }
 
     func testMultipleEntries() {
         let cacheControl = CacheControl([
-			(MediaType.textCss, [.noStore]),
-			(MediaType.text, [.noCache, .public]),
-		])
+            (MediaType.textCss, [.noStore]),
+            (MediaType.text, [.noCache, .public]),
+        ])
         XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-store")
     }
 
     func testCssIsAny() {
         let cacheControl = CacheControl([
-			(MediaType(type: .any), [.noCache, .public]),
-		])
+            (MediaType(type: .any), [.noCache, .public])
+        ])
         XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-cache, public")
     }
 }
