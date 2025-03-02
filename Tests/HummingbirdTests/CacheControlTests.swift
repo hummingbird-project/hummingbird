8219ec6cb484941c4195648b961cdb83a3d1290e
Benedek Kozma <cyberbeni@gmail.com>
2025-03-02 17:53:46 +0100

    Add CacheControl tests (#680)

---
+++ Tests/HummingbirdTests/CacheControlTests.swift
@@ -0,0 +1,26 @@
+import Hummingbird
+import XCTest
+
+final class CacheControlTests: XCTestCase {
+    func testCssIsText() {
+        let cacheControl = CacheControl([
+            (MediaType(type: .text), [.noCache, .public])
+        ])
+        XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-cache, public")
+    }
+
+    func testMultipleEntries() {
+        let cacheControl = CacheControl([
+            (MediaType.textCss, [.noStore]),
+            (MediaType.text, [.noCache, .public]),
+        ])
+        XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-store")
+    }
+
+    func testCssIsAny() {
+        let cacheControl = CacheControl([
+            (MediaType(type: .any), [.noCache, .public])
+        ])
+        XCTAssertEqual(cacheControl.getCacheControlHeader(for: "test.css"), "no-cache, public")
+    }
+}
