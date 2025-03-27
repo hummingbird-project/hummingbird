a7ae7dfbf853838772cc8f84fe216b9612bd319a
Adam Fowler <adamfowler71@gmail.com>
2025-03-27 17:21:02 +0000

    Add function headers

--- Sources/Hummingbird/Server/Request.swift
+++ Sources/Hummingbird/Server/Request.swift
@@ -67,9 +67,12 @@ extension Request {
     /// Conditional request which will only be processed if the eTag supplied is not in the
     /// `If-None-Match` request header.
     ///
+    /// For GET and HEAD methods, to update a cached entity that has an associated ETag.
+    ///
     /// - Parameters:
     ///   - headers: Headers to add to response when condition fails
     ///   - eTag: ETag for this responses content
+    ///   - context: Request context
     ///   - process: Closure to run if eTag is not in the `If-None-Match` header
     /// - Returns: Response
     public func ifNoneMatch(
@@ -98,6 +101,22 @@ extension Request {
         return response
     }
 
+    /// Conditional request which will only be processed if the eTag supplied is in the
+    /// `If-Match` request header.
+    ///
+    /// - For GET and HEAD methods, used in combination with a Range header, it can guarantee
+    ///   that the new ranges requested come from the same resource as the previous one.
+    /// - For other methods, and in particular for PUT, If-Match can be used to prevent
+    ///   the lost update problem. It can check if the modification of a resource that the
+    ///   user wants to upload will not override another change that has been done since
+    ///   the original resource was fetched.
+    ///
+    /// - Parameters:
+    ///   - headers: Headers to add to response when condition fails
+    ///   - eTag: ETag for this responses content
+    ///   - context: Request context
+    ///   - process: Closure to run if eTag is not in the `If-None-Match` header
+    /// - Returns: Response
     public func ifMatch(
         headers: HTTPFields = [:],
         eTag: String,
@@ -114,6 +133,17 @@ extension Request {
         return response
     }
 
+    /// Conditional request which will only be processed if the modification date supplied is after
+    /// the date in the `If-Modified-Since` request header.
+    ///
+    /// The most common use case is to update a cached entity that has no associated ETag.
+    ///
+    /// - Parameters:
+    ///   - headers: Headers to add to response when condition fails
+    ///   - modificationDate: Modification date for this responses content
+    ///   - context: Request context
+    ///   - process: Closure to run if eTag is not in the `If-None-Match` header
+    /// - Returns: Response
     public func ifModifiedSince(
         headers: HTTPFields = [:],
         modificationDate: Date,
@@ -140,6 +170,19 @@ extension Request {
         return response
     }
 
+    /// Conditional request which will only be processed if the modification date supplied is before
+    /// the date in the `If-Unmodified-Since` request header.
+    ///
+    /// In conjunction with non-safe methods like POST, this header can be used to implement an
+    /// optimistic concurrency control, as is done by some wikis: revision are rejected if the
+    /// stored document has been modified since the original was retrieved, avoiding conflicts.
+    ///
+    /// - Parameters:
+    ///   - headers: Headers to add to response when condition fails
+    ///   - modificationDate: Modification date for this responses content
+    ///   - context: Request context
+    ///   - process: Closure to run if eTag is not in the `If-None-Match` header
+    /// - Returns: Response
     public func ifUnmodifiedSince(
         headers: HTTPFields = [:],
         modificationDate: Date,
