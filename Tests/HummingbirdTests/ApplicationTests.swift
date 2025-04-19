2bdf0024686f06d8cd6becf02fe654f174194c65
Adam Fowler <adamfowler71@gmail.com>
2025-04-19 12:18:50 +0100

    Don't return response on HTTPParserError

--- Tests/HummingbirdTests/ApplicationTests.swift
+++ Tests/HummingbirdTests/ApplicationTests.swift
@@ -12,6 +12,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+import AsyncHTTPClient
 import Atomics
 import Foundation
 import HTTPTypes
@@ -25,6 +26,7 @@ import NIOCore
 import NIOEmbedded
 import NIOSSL
 import ServiceLifecycle
+import Synchronization
 import XCTest
 
 @testable import Hummingbird
@@ -1158,6 +1160,67 @@ final class ApplicationTests: XCTestCase {
                 XCTAssertEqual(response.status, .ok)
             }
         }
+    }
+
+    @available(macOS 15, *)
+    func testCancelledRequest() async throws {
+        let httpClient = HTTPClient()
+        let (stream, cont) = AsyncStream.makeStream(of: Int.self)
+
+        let router = Router()
+        router.post("/") { request, context in
+            let b = try await request.body.collect(upTo: .max)
+            return Response(status: .ok, body: .init(byteBuffer: b))
+        }
+        let app = Application(
+            router: router,
+            onServerRunning: { cont.yield($0.localAddress!.port!) }
+        )
+        do {
+            try await withThrowingTaskGroup(of: Void.self) { group in
+                let serviceGroup = ServiceGroup(
+                    configuration: .init(
+                        services: [app],
+                        gracefulShutdownSignals: [.sigterm, .sigint],
+                        logger: Logger(label: "SG")
+                    )
+                )
+
+                group.addTask {
+                    try await serviceGroup.run()
+                }
+
+                let port = await stream.first { _ in true }!
+                let task = Task {
+                    let count = Atomic(0)
+                    let stream = AsyncStream {
+                        let values = count.add(1, ordering: .relaxed)
+                        if values.oldValue < 16 {
+                            try? await Task.sleep(for: .milliseconds(100))
+                            return ByteBuffer(repeating: 0, count: 256)
+                        } else {
+                            return nil
+                        }
+                    }
+                    var request = HTTPClientRequest(url: "http://localhost:\(port)")
+                    request.method = .POST
+                    request.body = .stream(stream, length: .known(Int64(4096)))
+                    let response = try await httpClient.execute(request, deadline: .now() + .minutes(30))
+                    let result = try await response.body.collect(upTo: .max)
+                    print("Result size: \(result.readableBytes)")
+                }
+
+                try await Task.sleep(for: .seconds(1))
+                task.cancel()
+                try await Task.sleep(for: .seconds(1))
+                //                _ = try await group.next()
+                await serviceGroup.triggerGracefulShutdown()
+            }
+        } catch {
+            try await httpClient.shutdown()
+            throw error
+        }
+        try await httpClient.shutdown()
     }
 }
 
