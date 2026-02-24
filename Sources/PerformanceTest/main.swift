658c26c1606ca70ef22e7b9617a0717b67433e61
Adam Jansch <dev@adamjansch.co.uk>
2026-02-24 13:04:12 +0000

    Issue 781 - Crash when trying to run with Hummingbird library install on iOS 15 and 16
    Run swift format on code.

--- Sources/PerformanceTest/main.swift
+++ Sources/PerformanceTest/main.swift
@@ -16,43 +16,43 @@ if #available(macOS 14, iOS 17, tvOS 17, *) {
     let env = Environment()
     let hostname = env.get("SERVER_HOSTNAME") ?? "127.0.0.1"
     let port = env.get("SERVER_PORT", as: Int.self) ?? 8080
-    
+
     // create app
     let elg = MultiThreadedEventLoopGroup(numberOfThreads: 4)
     var router = Router()
     router.addMiddleware {
         FileMiddleware()
     }
-    
+
     // number of raw requests
     // ./wrk -c 128 -d 15s -t 8 http://localhost:8080
     router.get { _, _ in
         "Hello, world"
     }
-    
+
     // request with a body
     // ./wrk -c 128 -d 15s -t 8 -s scripts/post.lua http://localhost:8080
     router.post { request, _ in
         Response(status: .ok, body: .init(asyncSequence: request.body))
     }
-    
+
     struct Object: ResponseEncodable {
         let message: String
     }
-    
+
     // return JSON
     // ./wrk -c 128 -d 15s -t 8 http://localhost:8080/json
     router.get("json") { _, _ in
         Object(message: "Hello, world")
     }
-    
+
     // return JSON
     // ./wrk -c 128 -d 15s -t 8 http://localhost:8080/json
     router.get("wait") { _, _ in
         try await Task.sleep(for: .seconds(8))
         return "I waited"
     }
-    
+
     var app = Application(
         responder: router.buildResponder(),
         configuration: .init(
@@ -62,9 +62,9 @@ if #available(macOS 14, iOS 17, tvOS 17, *) {
         eventLoopGroupProvider: .shared(elg)
     )
     app.logger.logLevel = .debug
-    
+
     // configure app
-    
+
     // run app
     try await app.runService()
 } else {
