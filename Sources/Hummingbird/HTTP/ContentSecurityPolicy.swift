5636ff73901842adaa145755713ef9610420bbb1
Adam Fowler <adamfowler71@gmail.com>
2026-05-01 18:11:00 +0100

    Finish ContentSecurityPolicy support, add tests

--- Sources/Hummingbird/HTTP/ContentSecurityPolicy.swift
+++ Sources/Hummingbird/HTTP/ContentSecurityPolicy.swift
@@ -6,8 +6,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
-    public struct FetchDirective: Sendable {
+/// Helper to build content-security-policy header
+public struct ContentSecurityPolicy: Sendable, CustomStringConvertible, ExpressibleByDictionaryLiteral {
+    /// Content-security-policy directive
+    public struct Directive: Sendable, Hashable, CustomStringConvertible {
         enum Internal: String, Hashable {
             case childSrc = "child-src"
             case connectSrc = "connect-src"
@@ -41,41 +43,44 @@ public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
 
         let value: Internal
 
+        public var description: String { self.value.rawValue }
+
         /// Fetch directives
         ///
         /// Fetch directives control the locations from which certain resource types may be loaded.
 
-        /// Defines the valid sources for web workers and nested browsing contexts loaded using elements such as <frame> and <iframe>.
+        /// Defines the valid sources for web workers and nested browsing contexts loaded using elements such as
+        /// \<frame\> and \<iframe\>.
         public static var childSrc: Self { .init(value: .childSrc) }
         /// Restricts the URLs which can be loaded using script interfaces.
         public static var connectSrc: Self { .init(value: .connectSrc) }
         /// Serves as a fallback for the other fetch directives.
         public static var defaultSrc: Self { .init(value: .defaultSrc) }
-        /// Specifies valid sources for nested browsing contexts loaded into <fencedframe> elements.
+        /// Specifies valid sources for nested browsing contexts loaded into \<fencedframe\> elements.
         public static var fencedFrameSrc: Self { .init(value: .fencedFrameSrc) }
         /// Specifies valid sources for fonts loaded using @font-face.
         public static var fontSrc: Self { .init(value: .fontSrc) }
-        /// Specifies valid sources for nested browsing contexts loaded into elements such as <frame> and <iframe>.
+        /// Specifies valid sources for nested browsing contexts loaded into elements such as \<frame\> and \<iframe\>.
         public static var frameSrc: Self { .init(value: .frameSrc) }
         /// Specifies valid sources of images and favicons.
         public static var imgSrc: Self { .init(value: .imgSrc) }
         /// Specifies valid sources of application manifest files.
         public static var manifestSrc: Self { .init(value: .manifestSrc) }
-        /// Specifies valid sources for loading media using the <audio>, <video> and <track> elements.
+        /// Specifies valid sources for loading media using the \<audio\>, \<video\> and \<track\> elements.
         public static var mediaSrc: Self { .init(value: .mediaSrc) }
-        /// Specifies valid sources for the <object> and <embed> elements.
+        /// Specifies valid sources for the \<object\> and \<embed\> elements.
         public static var objectSrc: Self { .init(value: .objectSrc) }
         /// Specifies valid sources to be prefetched or prerendered.
         public static var prefetchSrc: Self { .init(value: .prefetchSrc) }
         /// Specifies valid sources for JavaScript and WebAssembly resources.
         public static var scriptSrc: Self { .init(value: .scriptSrc) }
-        /// Specifies valid sources for JavaScript <script> elements.
+        /// Specifies valid sources for JavaScript \<script\> elements.
         public static var scriptSrcElem: Self { .init(value: .scriptSrcElem) }
         /// Specifies valid sources for JavaScript inline event handlers.
         public static var scriptSrcAttr: Self { .init(value: .scriptSrcAttr) }
         /// Specifies valid sources for stylesheets.
         public static var styleSrc: Self { .init(value: .styleSrc) }
-        /// Specifies valid sources for stylesheets <style> elements and <link> elements with rel="stylesheet".
+        /// Specifies valid sources for stylesheets \<style\> elements and \<link\> elements with rel="stylesheet".
         public static var styleSrcElem: Self { .init(value: .styleSrcElem) }
         /// Specifies valid sources for inline styles applied to individual DOM elements.
         public static var styleSrcAttr: Self { .init(value: .styleSrcAttr) }
@@ -86,9 +91,9 @@ public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
         ///
         /// Document directives govern the properties of a document or worker environment to which a policy applies.
 
-        /// Restricts the URLs which can be used in a document's <base> element.
+        /// Restricts the URLs which can be used in a document's \<base\> element.
         public static var baseUri: Self { .init(value: .baseUri) }
-        /// Enables a sandbox for the requested resource similar to the <iframe> sandbox attribute.
+        /// Enables a sandbox for the requested resource similar to the \<iframe\> sandbox attribute.
         public static var sandbox: Self { .init(value: .sandbox) }
 
         /// Navigation directives
@@ -97,7 +102,7 @@ public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
 
         /// Restricts the URLs which can be used as the target of a form submissions from a given context.
         public static var formAction: Self { .init(value: .formAction) }
-        /// Specifies valid parents that may embed a page using <frame>, <iframe>, <object>, or <embed>.
+        /// Specifies valid parents that may embed a page using \<frame\>, \<iframe\>, \<object\>, or \<embed\>.
         public static var formAncestors: Self { .init(value: .formAncestors) }
 
         /// Reporting directives
@@ -127,19 +132,49 @@ public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
         public static var reportUri: Self { .init(value: .reportUri) }
     }
 
-    public struct FetchDirectiveValue: Sendable, CustomStringConvertible {
-        enum Internal {
+    /// Content-security-policy value scheme
+    public struct Scheme: Sendable {
+        public let rawValue: String
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public static var http: Self { .init(rawValue: "http:") }
+        public static var https: Self { .init(rawValue: "https:") }
+        public static var ws: Self { .init(rawValue: "ws:") }
+        public static var wss: Self { .init(rawValue: "wss:") }
+    }
+
+    /// Content-security-policy value hash
+    public struct HashAlgorithm: Sendable {
+        public let rawValue: String
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public static var sha256: Self { .init(rawValue: "sha256") }
+        public static var sha384: Self { .init(rawValue: "sha384") }
+        public static var sha512: Self { .init(rawValue: "sha512") }
+    }
+
+    /// Content-security-policy directive value
+    ///
+    /// These are to be used with fetch directives. All other directives expect a raw string eg
+    /// ```swift
+    /// let csp: ContentSecurityPolicy = [
+    ///     .defaultSrc: [.self],
+    ///     .reportTo: ["csp-reports"]
+    /// ]
+    /// ```
+    public struct DirectiveValue: Sendable, CustomStringConvertible, ExpressibleByStringLiteral {
+        enum Internal: Sendable {
             case none
             case `self`
-            case nonce(base64: String)
-            case sha256(base64: String)
-            case sha384(base64: String)
-            case sha512(base64: String)
-            case hostSource(String)
-            case http
-            case https
-            case ws
-            case wss
+            case nonce(String)
+            case hash(HashAlgorithm, String)
+            case raw(String)
             case trustedTypesEval
             case unsafeEval
             case wasmUnsafeEval
@@ -151,38 +186,118 @@ public struct ContentSecurityPolicy: Sendable, CustomStringConvertible {
         }
         let value: Internal
 
+        /// The single value 'none', indicating that the specific resource type should be completely blocked
+        public static var none: Self { .init(.none) }
+        /// Resources of the given type may only be loaded from the same origin as the document.
+        public static var `self`: Self { .init(.self) }
+        /// This value consists of the string nonce- followed by a nonce value. The nonce value may use any of the
+        /// characters from Base64 or URL-safe Base64.
+        public static func nonce(_ base64: String) -> Self { .init(.nonce(base64)) }
+        /// This value consists of a string identifying a hash algorithm, followed by -, followed by a hash value.
+        /// The hash value may use any of the characters from Base64 or URL-safe Base64.
+        public static func hash(_ algorithm: HashAlgorithm, base64: String) -> Self { .init(.hash(algorithm, base64)) }
+        /// The URL or IP address of a host that is a valid source for the resource.
+        /// The scheme, port number, and path are optional.
+        /// If the scheme is omitted, the scheme of the document's origin is used.
+        public static func uri(_ uri: String) -> Self { .init(.raw(uri)) }
+        /// A scheme, such as https:
+        public static func scheme(_ scheme: Scheme) -> Self { .init(.raw(scheme.rawValue)) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then JavaScript functions which
+        /// evaluate their arguments as JavaScript are disabled. This includes eval(), the code argument to setTimeout(),
+        /// or the Function() constructor.
+        ///
+        /// The trusted-types-eval keyword can be used to undo this protection, but only when Trusted Types are enforced
+        /// and passed to these functions instead of strings. This allows dynamic evaluation of strings as JavaScript,
+        /// but only after inputs have been passed through a transformation function before it is injected, which has
+        /// the chance to sanitize the input to remove potentially dangerous markup.
+        public static var trustedTypesEval: Self { .init(.trustedTypesEval) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then JavaScript functions which
+        /// evaluate their arguments as JavaScript are disabled. This includes eval(), the code argument to setTimeout(),
+        /// or the Function() constructor.
+        ///
+        /// The unsafe-eval keyword can be used to undo this protection, allowing dynamic evaluation of strings as
+        /// JavaScript.
+        /// > Developers should avoid using `unsafe-eval`
+        public static var unsafeEval: Self { .init(.unsafeEval) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then a page won't be allowed to
+        /// compile WebAssembly using functions like WebAssembly.compileStreaming().
+        ///
+        /// The wasm-unsafe-eval keyword can be used to undo this protection. This is a much safer alternative to
+        /// 'unsafe-eval', since it does not enable general evaluation of JavaScript.
+        public static var wasmUnsafeEval: Self { .init(.wasmUnsafeEval) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then inline JavaScript is not
+        /// allowed to execute.
+        ///
+        /// The unsafe-inline keyword can be used to undo this protection, allowing all these forms to be loaded.
+        /// > Developers should avoid using `unsafe-inline`
+        public static var unsafeInline: Self { .init(.unsafeInline) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then inline event handler attributes
+        /// like onclick and inline style attributes are not allowed to execute.
+        ///
+        /// The 'unsafe-hashes' expression allows the browser to use hash expressions for inline event handlers
+        /// and style attributes.
+        public static var unsafeHashes: Self { .init(.unsafeHashes) }
+        /// By default, if a CSP contains a default-src or a script-src directive, then inline JavaScript is not
+        /// allowed to execute. The 'inline-speculation-rules' allows the browser to load inline \<script\> elements
+        /// that have a type attribute of speculationrules.
+        public static var inlineSpeculationRules: Self { .init(.inlineSpeculationRules) }
+        /// The 'strict-dynamic' keyword makes the trust conferred on a script by a nonce or a hash extend to scripts
+        /// that this script dynamically loads, for example by creating new \<script\> tags using Document.createElement()
+        /// and then inserting them into the document using Node.appendChild().
+        public static var strictDynamic: Self { .init(.strictDynamic) }
+        /// If this expression is included in a directive controlling scripts or styles, and the directive causes the
+        /// browser to block any inline scripts, inline styles, or event handler attributes, then the violation report
+        /// that the browser generates will contain a sample property containing the first 40 characters of the blocked
+        /// resource.
+        public static var reportSample: Self { .init(.reportSample) }
+
         public var description: String {
             switch self.value {
             case .none: "'none'"
             case .self: "'self'"
             case .nonce(let string): "'nonce-\(string)'"
-            case .sha256(let string): "'sha256-\(string)'"
-            case .sha384(let string): "'sha384-\(string)'"
-            case .sha512(let string): "'sha512-\(string)'"
-            case .hostSource(let string): string
-            case .http: "http:"
-            case .https: "https:"
-            case .ws: "ws:"
-            case .wss: "wss."
-            case .trustedTypesEval: "trusted-types-eval"
-            case .unsafeEval: "unsafe-eval"
-            case .wasmUnsafeEval: "wasm-unsafe-eval"
-            case .unsafeInline: "unsafe-inline"
-            case .unsafeHashes: "unsafe-hashes"
-            case .inlineSpeculationRules: "inline-speculation-rules"
-            case .strictDynamic: "strict-dynamic"
-            case .reportSample: "report-sample"
+            case .hash(let algorithm, let string): "'\(algorithm.rawValue)-\(string)'"
+            case .trustedTypesEval: "'trusted-types-eval'"
+            case .unsafeEval: "'unsafe-eval'"
+            case .wasmUnsafeEval: "'wasm-unsafe-eval'"
+            case .unsafeInline: "'unsafe-inline'"
+            case .unsafeHashes: "'unsafe-hashes'"
+            case .inlineSpeculationRules: "'inline-speculation-rules'"
+            case .strictDynamic: "'strict-dynamic'"
+            case .reportSample: "'report-sample'"
+            case .raw(let string): string
             }
         }
-    }
-    public struct PolicyDirective: Sendable {
-        let value: String
 
-        public init(_ fetchDirective: FetchDirective, values: [FetchDirectiveValue]) {
-            self.value = "\(fetchDirective) \(values)"
+        init(_ value: Internal) {
+            self.value = value
+        }
+        public init(stringLiteral value: String) {
+            self.value = .raw(value)
         }
     }
-    let policyDirectives: [PolicyDirective]
+    let policyDirectives: [(Directive, [DirectiveValue])]
 
-    public var description: String { self.policyDirectives.map { $0.value }.joined(separator: "; ") }
+    ///  Initialize Content Security Policy from an array of directive and directive value array pairs
+    /// - Parameter policyDirectives: Array of directive and directive value array pairs
+    public init(_ policyDirectives: [(Directive, [DirectiveValue])]) {
+        self.policyDirectives = policyDirectives
+    }
+
+    ///  Initialize Content Security Policy from a dictionary of directive and directive value array pairs
+    /// - Parameter policyDirectives: Dictionary of directive and directive value array pairs
+    public init(_ policyDirectives: [Directive: [DirectiveValue]]) {
+        self.policyDirectives = policyDirectives.map { $0 }
+    }
+
+    ///  Initialize Content Security Policy from a dictionary literal of directive and directive value array pairs
+    /// - Parameter policyDirectives: Array of directive and directive value array pairs
+    public init(dictionaryLiteral elements: (Directive, [DirectiveValue])...) {
+        self.policyDirectives = elements
+    }
+
+    /// Formatted output for content-security-policy header
+    public var description: String {
+        self.policyDirectives.map { "\($0.0) \($0.1.map { $0.description }.joined(separator: " "))" }.joined(separator: "; ")
+    }
 }
