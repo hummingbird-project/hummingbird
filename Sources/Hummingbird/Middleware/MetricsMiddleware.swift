dfadcff2df9274db2abbbd41c67040aa4277ef7c
Adam Fowler <adamfowler71@gmail.com>
2026-07-07 13:50:21 +0100

    Add a metrics cache to MetricsMiddleware
    This should improve the performance of finding metrics primitives for particular endpoints.

--- Sources/Hummingbird/Middleware/MetricsMiddleware.swift
+++ Sources/Hummingbird/Middleware/MetricsMiddleware.swift
@@ -8,6 +8,7 @@
 
 import Dispatch
 import Metrics
+import NIOConcurrencyHelpers
 
 /// Middleware recording metrics for each request
 ///
@@ -19,60 +20,116 @@ import Metrics
 ///
 /// A list of implementations is available in the swift-log repository's README.
 public struct MetricsMiddleware<Context: RequestContext>: RouterMiddleware {
-    public init() {}
+    let metricsCache: MetricsCache
+
+    public init() {
+        self.metricsCache = .init()
+    }
 
     public func handle(_ request: Request, context: Context, next: (Request, Context) async throws -> Response) async throws -> Response {
         let startTime = DispatchTime.now().uptimeNanoseconds
-        let activeRequestMeter = Meter(label: "http.server.active_requests", dimensions: [("http.request.method", request.method.description)])
+        let activeRequestMeter = self.metricsCache.getMethodMetrics(id: .init(method: request.method)).activeRequestMeter
         activeRequestMeter.increment()
         do {
             var response = try await next(request, context)
             let responseStatus = response.status
             response.body = response.body.withPostWriteClosure {
-                // need to create dimensions once request has been responded to ensure
-                // we have the correct endpoint path
-                let dimensions: [(String, String)] = [
-                    ("http.route", context.endpointPath ?? "Unknown"),
-                    ("http.request.method", request.method.rawValue),
-                    ("http.response.status_code", responseStatus.code.description),
-                ]
-                Counter(label: "hb.requests", dimensions: dimensions).increment()
-                Metrics.Timer(
-                    label: "http.server.request.duration",
-                    dimensions: dimensions,
-                    preferredDisplayUnit: .seconds
-                ).recordNanoseconds(DispatchTime.now().uptimeNanoseconds - startTime)
+                let metrics = self.metricsCache.getEndpointMetrics(
+                    id: .init(endpoint: context.endpointPath ?? "Unknown", method: request.method, status: responseStatus)
+                )
+                metrics.counter.increment()
+                metrics.timer.recordNanoseconds(DispatchTime.now().uptimeNanoseconds - startTime)
                 activeRequestMeter.decrement()
             }
             return response
         } catch {
-            let errorType: String
+            let errorType: HTTPResponse.Status
             if let httpError = error as? any HTTPResponseError {
-                errorType = httpError.status.code.description
+                errorType = httpError.status
             } else {
-                errorType = "500"
+                errorType = .internalServerError
             }
-            let endpointPath = context.endpointPath ?? "NotFound"
-            // increment requests
-            Counter(
-                label: "hb.requests",
-                dimensions: [
-                    ("http.route", endpointPath),
-                    ("http.request.method", request.method.rawValue),
-                    ("http.response.status_code", errorType),
-                ]
-            ).increment()
-            // increment errors
-            Counter(
-                label: "hb.request.errors",
-                dimensions: [
-                    ("http.route", endpointPath),
-                    ("http.request.method", request.method.rawValue),
-                    ("error.type", errorType),
-                ]
-            ).increment()
+            let metrics = self.metricsCache.getEndpointMetrics(
+                id: .init(endpoint: context.endpointPath ?? "NotFound", method: request.method, status: errorType)
+            )
+            metrics.counter.increment()
+            metrics.errorCounter.increment()
             activeRequestMeter.decrement()
             throw error
+        }
+    }
+}
+
+/// Cache to store, metrics associated with particular endpoints and methods.
+///
+/// This should provide a quicker lookup for metrics primitives than expecting the
+/// underlying metrics factory to do a general lookup
+final class MetricsCache: Sendable {
+    struct MethodMetrics: Sendable {
+        struct ID: Hashable {
+            let method: HTTPRequest.Method
+        }
+        let activeRequestMeter: Meter
+
+        init(id: ID) {
+            self.activeRequestMeter = Meter(label: "http.server.active_requests", dimensions: [("http.request.method", id.method.description)])
+        }
+    }
+    struct EndpointMetrics: Sendable {
+        struct ID: Hashable {
+            let endpoint: String
+            let method: HTTPRequest.Method
+            let status: HTTPResponse.Status
+        }
+        let counter: Counter
+        let timer: Timer
+        let errorCounter: Counter
+
+        init(id: ID) {
+            self.counter = Counter(
+                label: "hb.requests",
+                dimensions: [
+                    ("http.route", id.endpoint),
+                    ("http.request.method", id.method.description),
+                    ("http.response.status_code", id.status.code.description),
+                ]
+            )
+            self.timer = Timer(
+                label: "http.server.request.duration",
+                dimensions: [
+                    ("http.route", id.endpoint),
+                    ("http.request.method", id.method.description),
+                    ("http.response.status_code", id.status.code.description),
+                ],
+                preferredDisplayUnit: .seconds
+            )
+            self.errorCounter = Counter(
+                label: "hb.request.errors",
+                dimensions: [
+                    ("http.route", id.endpoint),
+                    ("http.request.method", id.method.description),
+                    ("error.type", id.status.code.description),
+                ]
+            )
+        }
+    }
+    let methodMetricsStorage: NIOLockedValueBox<[MethodMetrics.ID: MethodMetrics]>
+    let endpointMetricsStorage: NIOLockedValueBox<[EndpointMetrics.ID: EndpointMetrics]>
+
+    init() {
+        self.methodMetricsStorage = .init([:])
+        self.endpointMetricsStorage = .init([:])
+    }
+
+    func getMethodMetrics(id: MethodMetrics.ID) -> MethodMetrics {
+        self.methodMetricsStorage.withLockedValue { metricsMap in
+            metricsMap[id, default: .init(id: id)]
+        }
+    }
+
+    func getEndpointMetrics(id: EndpointMetrics.ID) -> EndpointMetrics {
+        self.endpointMetricsStorage.withLockedValue { metricsMap in
+            metricsMap[id, default: .init(id: id)]
         }
     }
 }
