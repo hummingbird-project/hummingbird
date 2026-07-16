5a39470fb94fedca157308b315e18d6d524cc51c
Adam Fowler <adamfowler71@gmail.com>
2026-07-16 09:55:48 +0100

    Disable file tests for Windows

--- Tests/HummingbirdTests/FileMiddlewareTests.swift
+++ Tests/HummingbirdTests/FileMiddlewareTests.swift
@@ -5,6 +5,7 @@
 // See LICENSE.txt for license information
 // SPDX-License-Identifier: Apache-2.0
 //
+#if !os(Windows)
 
 import Foundation
 import HTTPTypes
@@ -714,3 +715,5 @@ struct FileMiddlewareTests {
         }
     }
 }
+
+#endif
